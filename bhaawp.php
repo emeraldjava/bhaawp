d0e2f0c0a015790eec8b1b2d53192fec06f7e1d0
Paul O'Connell <paul.t.oconnell@gmail.com>
2012-05-25 16:48:40 +0100

    Add initial plugin file.

---
+++ bhaawp.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: BHAA wordpress plugin
+Plugin URI: https://github.com/emeraldjava/bhaawp
+Description: Plugin to handle bhaa results
+Version: 1.0.0
+Author: paul.t.oconnell@gmail.com
+Author URI: https://github.com/emeraldjava/bhaawp
+*/
+
+?>
