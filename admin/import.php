d1d4e7d7d097b560aaf1fcc80c8cc951b1934437
emeraldjava <paul.t.oconnell@gmail.com>
2012-08-03 17:50:03 +0100

    Import user

--- admin/import.php
+++ admin/import.php
@@ -1,11 +1,4 @@
 <?php
-// set_time_limit(300);
-// ini_set('memory_limit', '16M');
-
-// if(!class_exists('BhaaImport')){
-// 	require_once( ABSPATH . 'wp-admin/includes/import.php' );
-// }
-
 // http://core.trac.wordpress.org/attachment/ticket/3398/geeklog.php
 class BhaaImport
 {
@@ -26,13 +19,7 @@ class BhaaImport
 	 */
 	public function import()
 	{
-		if (empty ($_GET['action']))
-			$action = "";
-		else
-			$step = (int) $_GET['step'];
-	
 		$this->header();
-		
 		if(empty ($_GET['action']))
 			$this->greet();
 		elseif($_GET['action']=='events')
@@ -45,7 +32,6 @@ class BhaaImport
 			$this->deleteUsers();
 		else
 			$this->greet();
-		
 		$this->footer();
 	}
 	
@@ -136,24 +122,70 @@ class BhaaImport
         echo '<p>'.__('Importing Users...').'<br /><br /></p>';
 
         $users = $this->getUsers();
-        
+
+        require_once( ABSPATH . 'wp-includes/user.php' );
         foreach($users as $user)
         {
+
         	$count++;
         	/**
         	 * http://codex.wordpress.org/Function_Reference/wp_insert_user
         	 * http://codex.wordpress.org/Function_Reference/wp_create_user
         	 */
-//         	$ret_id = wp_insert_user(array(
-//         	             'ID'            => $user->id,
-//         		         'user_login'    => $user->email,
-//         		         'user_nicename' => $user->email,
-//         		         'user_email'    => $user->email,
-//         		         'user_registered' => 'NOW()',
-//         	             'display_name'  => $user->email));
+//         	id,
+//         	firstname,
+//         	surname,
+//         	email,
+//         	gender,
+//         	email,
+//         	dateofbirth,
+//         	company,
+//         	companyname,
+//         	team,
+//         	newsletter,
+//         	telephone,
+//         	mobilephone,
+//         	textmessage,
+//         	address1,
+//         	address2,
+//         	address3,
+//         	status,
+//         	insertdate,
+//         	dateofrenewal
+
+//        	wp_create_user( $username, $password, $email );
+
+        	// http://codex.wordpress.org/Function_Reference/wp_insert_user
+        	// INSERT INTO `wp_users`(`ID`) VALUES (7713);
+        	$id = wp_insert_user(array(
+        	        'ID'            => $user->id,
+        		    'user_login'    => $user->email,
+        			'user_pass' => wp_hash_password($user->id),
+        		    //'user_nicename' => $user->email,
+        			// ''user_url
+        		    'user_email'    => $user->email,
+        			'nickname' => $user->email,
+        			'display_name'=> $user->firstname.' '.$user->surname,
+        			'first_name' => $user->firstname,
+        			'last_name'=> $user->surname
+        	//	    'user_registered' => 'NOW()'
+				));
+								
+			if ( is_wp_error($id) )
+			{
+				error_log($id->get_error_message());
+				echo $id->get_error_message();
+        	}
+			else 
+			{
+				echo '<p>'.$id.'</p>';
+				error_log($id);
+			}
+			update_usermeta( $id, 'rich_editing', 'false');
+			
         	//echo '<p>'.$count.' - '.$user->id.' '.$ret_id.'</p>';
         	//echo '<p>emEvent '.$emEvent->post_id.' '.$emEvent->event_id.' '.$emLocation->location_id.'</p>';
-        	error_log(print_r($user));
+//        	error_log(print_r($user));
         	
         }
 	}
@@ -161,8 +193,6 @@ class BhaaImport
 	function getEvents()
 	{
 		global $wpdb;
-		//$gldb = new wpdb($this->dbusername,$this->dbpassword,$this->dbname,$this->dbhost);
-		//set_magic_quotes_runtime(0);
 		return $gldb->get_results('SELECT id,name,tag,date,location FROM event limit 25');//, ARRAY_A);
 	}
 	
@@ -187,9 +217,28 @@ class BhaaImport
     {
     	global $wpdb;
     	return $wpdb->get_results($wpdb->prepare(
-   			"SELECT id,firstname,surname,email,gender,email,dateofbirth,
-    			company,companyname,team,newsletter,telephone,mobilephone,textmessage,
-    			address1,address2,address3,status,insertdate,dateofrenewal FROM runner where ID = %d",7713));
+   			"SELECT 
+    			id,
+    			firstname,
+    			surname,
+    			email,
+    			gender,
+    			email,
+    			dateofbirth,
+    			company,
+    			companyname,
+    			team,
+    			newsletter,
+    			telephone,
+    			mobilephone,
+    			textmessage,
+    			address1,
+    			address2,
+    			address3,
+    			status,
+    			insertdate,
+    			dateofrenewal
+    		FROM runner where ID = %d",7713));
 	}
 	
 	function deleteUsers()
@@ -201,7 +250,4 @@ class BhaaImport
 			"DELETE FROM wp_usermeta WHERE user_id != %d",1));
 	}
 }
-//$import = new BhaaImport();
-//register_importer('bhaaimport', 'BhaaImport', __('Import bhaa details'), array ($import, 'dispatch'));
-
 ?>
