14dc8e5817a479116c64ec1bfa6b613cb465d94f
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-06 18:45:22 +0100

    Add custom user profile fields.

--- bhaawp.php
+++ bhaawp.php
@@ -54,6 +54,26 @@ class BhaaLoader
 		{
 			$this->addShortCodes();
 		}		
+	
+		add_action('show_user_profile',array(&$this, 'add_extra_profile_fields'));
+		add_action('edit_user_profile',array(&$this, 'extra_user_profile_fields'));
+		
+	}
+	
+	// http://bavotasan.com/2009/adding-extra-fields-to-the-wordpress-user-profile/
+	function add_extra_profile_fields($user) {
+		
+		$status = get_usermeta($user->ID,'bhaa_runner_address1');
+		echo '<h3>BHAA Data</h3>';
+		echo '<table class="form-table">
+			<tr>
+			<th><label for="address">Address</label></th>
+			<td>
+			<input type="text" name="address" id="address" value="'.$status.'" class="regular-text" /><br />
+			<span class="description">Please enter your address.</span>
+			</td>
+			</table>';
+		// add extra profile fields to user edit page
 	}
 	
 	function bhaa_connection_types() {
