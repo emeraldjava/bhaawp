ae015da2afaa7b182031cb098b81de6198f52cc0
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-11 16:12:11 +0100

    List the linked bhaa race cpt objects.

--- themes/BHAA/plugins/events-manager/templates/event-single.php
+++ themes/BHAA/plugins/events-manager/templates/event-single.php
@@ -15,3 +15,27 @@ global $EM_Event;
 /* @var $EM_Event EM_Event */
 echo $EM_Event->output_single();
 ?>
+
+<?php
+// Find connected pages
+$connected = new WP_Query( array(
+  'connected_type' => 'event_to_race',
+  'connected_items' => get_queried_object(),
+  'nopaging' => true,
+) );
+
+// Display connected pages
+if ( $connected->have_posts() ) :
+?>
+<h2>BHAA Races</h2>
+<ul>
+<?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
+	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+<?php endwhile; ?>
+</ul>
+
+<?php
+// Prevent weirdness
+wp_reset_postdata();
+endif;
+?>
