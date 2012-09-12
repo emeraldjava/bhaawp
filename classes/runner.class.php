5db8ed3a0866a30f101e20145ff6119df642ce29
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-12 14:02:15 +0100

    Add function to save the meta fields.

--- classes/runner.class.php
+++ classes/runner.class.php
@@ -8,8 +8,11 @@ class Runner
 {
 	function __construct()
 	{
-		add_action('show_user_profile',array(&$this, 'add_extra_profile_fields'));
-		add_action('edit_user_profile',array(&$this, 'extra_user_profile_fields'));
+		add_action('show_user_profile',array(&$this,'add_bhaa_profile_fields'));
+		add_action('edit_user_profile',array(&$this,'add_bhaa_profile_fields'));
+		
+		add_action('personal_options_update',array(&$this,'save_bhaa_user_profile_fields'));
+		add_action('edit_user_profile_update',array(&$this,'save_bhaa_user_profile_fields'));
 	}
 	
 	function Runner()
@@ -18,7 +21,7 @@ class Runner
 	}
 	
 	// http://bavotasan.com/2009/adding-extra-fields-to-the-wordpress-user-profile/
-	function add_extra_profile_fields($user) {
+	function add_bhaa_profile_fields($user) {
 	
 		$bhaa_runner_address1 = get_usermeta($user->ID,'bhaa_runner_address1');
 		echo '<h3>BHAA Data</h3>';
@@ -26,20 +29,32 @@ class Runner
 		<tr>
 		<th><label for="address">Address 1</label></th>
 		<td>
-		<input type="text" name="address" id="address" value="'.$bhaa_runner_address1.'" class="regular-text" /><br />
+		<input type="text" name="bhaa_runner_address1" id="bhaa_runner_address1" value="'.$bhaa_runner_address1.'" class="regular-text" /><br />
 		<span class="description">Please enter your address.</span>
 		</td>
 		</table>';
 		
 		echo '<table class="form-table">
 		<tr>
-		<th><label for="address">Address 2</label></th>
+		<th><label for="bhaa_runner_address2">Address 2</label></th>
 		<td>
-		<input type="text" name="address" id="address" value="'.get_usermeta($user->ID,'bhaa_runner_address2').'" class="regular-text" /><br />
+		<input type="text" name="bhaa_runner_address2" id="bhaa_runner_address2" value="'.get_usermeta($user->ID,'bhaa_runner_address2').'" class="regular-text" /><br />
 		<span class="description">Please enter your address.</span>
 		</td>
 		</table>';
 		// add extra profile fields to user edit page
+	}
+	
+	function save_bhaa_user_profile_fields( $user_id ) {
+	
+		if ( !current_user_can( 'edit_user', $user_id ) ) {
+			return false;
+		}
+	
+		update_user_meta( $user_id, 'bhaa_runner_address1', $_POST['bhaa_runner_address1'] );
+		update_user_meta( $user_id, 'bhaa_runner_address2', $_POST['bhaa_runner_address2'] );
+		//update_user_meta( $user_id, 'province', $_POST['province'] );
+		//update_user_meta( $user_id, 'postalcode', $_POST['postalcode'] );
 	}
 }
 ?>
