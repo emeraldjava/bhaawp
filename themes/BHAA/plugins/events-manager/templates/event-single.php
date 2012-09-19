b52ee72550b812373c11161e2fd2315ed321f501
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-19 19:34:00 +0100

    Update the event manager template to handle future and past events. Show results on past events.

--- themes/BHAA/plugins/events-manager/templates/event-single.php
+++ themes/BHAA/plugins/events-manager/templates/event-single.php
@@ -3,8 +3,7 @@
  * Remember that this file is only used if you have chosen to override event pages with formats in your event settings!
  * You can also override the single event page completely in any case (e.g. at a level where you can control sidebars etc.), as described here - http://codex.wordpress.org/Post_Types#Template_Files
  * Your file would be named single-event.php
- */
-/*
+ * 
  * This page displays a single event, called during the em_content() if this is an event page.
  * You can override the default display settings pages by copying this file to yourthemefolder/plugins/events-manager/templates/ and modifying it however you need.
  * You can display events however you wish, there are a few variables made available to you:
@@ -13,33 +12,35 @@
  */
 global $EM_Event;
 /* @var $EM_Event EM_Event */
-echo $EM_Event->output_single();
-?>
-
-<?php
-// Find connected pages
-$connected = new WP_Query( array(
-  'connected_type' => 'event_to_race',
-  'connected_items' => get_queried_object(),
-  'nopaging' => true,
-) );
-
-// Display connected pages
-if ( $connected->have_posts() ) :
-?>
-<h2>See the Race Results</h2>
-<ul>
-<?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
-	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-<?php endwhile; ?>
-</ul>
-
-<?php
-// Prevent weirdness
-wp_reset_postdata();
-else :
+if($EM_Event->start >= time())
 {
-	echo "No races have been linked to this event yet.";
+	echo '<h1>A future BHAA event</h1>';
+	echo $EM_Event->output_single();
 }
-endif;
+else
+{
+	echo '<h1>A past BHAA event</h1>';
+	echo '<ul>TODO<li>embed Youtube</li><li>embed flickr</li><li>Show top 3 three and total number of runners</li></ul>';
+	
+	// Find connected pages
+	$connected = new WP_Query( array(
+		'connected_type' => 'event_to_race',
+		'connected_items' => get_queried_object(),
+		'nopaging' => true,
+	));
+	
+	if ( $connected->have_posts() ) :
+	
+		echo '<h2>See the Full Race Results</h2><ul>';
+		while ( $connected->have_posts() ) : $connected->the_post();
+			echo '<li><a href="'.the_permalink().'">'.the_title().'</a></li>';
+		endwhile;
+		echo '</ul>';
+		
+		// Prevent weirdness
+		wp_reset_postdata();
+	else :
+		echo "No races have been linked to this event yet.";
+	endif;
+}
 ?>
