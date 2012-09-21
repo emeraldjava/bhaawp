77bc12af210e996b785434093abbb56e21a131b9
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-21 12:37:25 +0100

    Check the ticket name for annual membership.

--- themes/BHAA/plugins/events-manager/emails/bhaatickets.php
+++ themes/BHAA/plugins/events-manager/emails/bhaatickets.php
@@ -15,14 +15,34 @@ case '#_BHAATICKETS':
 -->
 
 <?php foreach($EM_Booking->get_tickets_bookings() as $EM_Ticket_Booking): ?>
+BHAA Ticket
 <?php
 /* @var $EM_Ticket_Booking EM_Ticket_Booking */
-echo $EM_Ticket_Booking->get_ticket()->name; 
+//echo $EM_Ticket_Booking->get_ticket()->name; 
+if($EM_Ticket_Booking->get_ticket()->name=='Annual Membership')
+{
 ?>
+Thanks #_CONTACTNAME,
 
-Quantity: <?php echo $EM_Ticket_Booking->get_spaces(); ?>
+Your now a BHAA annual member. ID #_CONTACTID
+<?php 
+}
+else 
+{
+?>
+Hi #_CONTACTNAME,
 
-Price: <?php echo em_get_currency_symbol(!get_option('dbem_smtp_html'))." ". number_format($EM_Ticket_Booking->get_price(),2); ?>
+BHAA event 
+When : #_EVENTDATES @ #_EVENTTIMES
+
+Where : #_LOCATIONNAME - #_LOCATIONFULLLINE
+
+Yours faithfully,
+
+#_CONTACTNAME
+<?php 
+}
+?>
 
 <?php endforeach; ?>
 
