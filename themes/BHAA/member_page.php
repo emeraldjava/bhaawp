322740a579205e8dba448b720f5c283eb4033e18
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-21 14:54:37 +0100

    Add a member template page.

---
+++ themes/BHAA/member_page.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+ Template Name: BHAA Member Page
+
+ http://www.expand2web.com/blog/custom-page-template-wordpress
+*/
+?>
+<?php get_header(); ?>
+BHAA Runner Page
+<?php get_footer(); ?>
