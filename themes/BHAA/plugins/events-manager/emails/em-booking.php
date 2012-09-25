abf9f0c72b5893de046d60ac1fcef58e867dab15
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-25 19:34:03 +0100

    Add the BHAATICKET hook.

--- themes/BHAA/plugins/events-manager/emails/em-booking.php
+++ themes/BHAA/plugins/events-manager/emails/em-booking.php
@@ -562,6 +562,11 @@ class EM_Booking extends EM_Object{
 					em_locate_template('emails/bookingtickets.php', true, array('EM_Booking'=>$this));
 					$replace = ob_get_clean();
 					break;
+				case '#_BHAATICKETS':
+					ob_start();
+					em_locate_template('emails/bhaatickets.php', true, array('EM_Booking'=>$this));
+					$replace = ob_get_clean();
+					break;
 				default:
 					$replace = $full_result;
 					break;
