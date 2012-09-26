4bf1f0fb0f4f0a3903fb358c27e4e68e8fed9195
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-26 16:55:50 +0100

    Add a RaceDay class to handle registration on the day (members,day,list, export)

---
+++ classes/raceday.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * handles the raceday registration application
+ * 
+ * User Search
+ * - http://www.blackbam.at/blackbams-blog/2011/06/27/wordpress-improved-user-search-first-name-last-name-email-in-backend/
+ * - http://plugins.svn.wordpress.org/improved-user-search-in-backend/tags/1.2.3/improved-user-search-in-backend.php
+ */
+class RaceDay
+{
+	function RaceDay()
+	{}
+}
+?>
