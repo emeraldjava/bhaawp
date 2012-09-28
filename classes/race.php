f1e4d4a1c26f7de8adda82e7260ed1c7964f1c1f
emeraldjava <paul.t.oconnell@gmail.com>
2012-09-28 18:47:25 +0100

    race php file

--- classes/race.php
+++ classes/race.php
@@ -15,6 +15,22 @@ class Race
 				'\n'   # Escape char
 				);
 		var_dump( $parsed );
+
+		var_dump( time() );
+// 		$headers = 'MIME-Version: 1.0' . "\r\n";
+// 		$headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
+// 		$headers .= 'From: sender@sender.com' . "\r\n";
+// 		mail("paul.oconnell@aegon.ie","test subject 3","test body 3",$headers);
+
+		$args = array(
+				'post_type'=> 'company',
+				'name'    => $_REQUEST['term']);
+		error_log('bhaawp_company_search '.$_REQUEST['term']);
+		//		query_posts($args);
+		
+		$query = new WP_Query($args);//'pagename=contact' );
+		
+		error_log('bhaawp_company_search '.$query);
 	}
 }
 
