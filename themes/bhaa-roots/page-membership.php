2432dc454eda4aaaa1d3972ae3f254233a28a920
emeraldjava <paul.t.oconnell@gmail.com>
2012-10-10 18:46:44 +0100

    Add is_user_logged_in logic.

--- themes/bhaa-roots/page-membership.php
+++ themes/bhaa-roots/page-membership.php
@@ -7,6 +7,16 @@
 ?>
 
 <?php get_header(); ?>
-BHAA Membership Page
+
+<?php 
+if(!is_user_logged_in())
+{
+	echo '<div>BHAA Membership Page - Please use the registration form to create a userr account.</div>';
+}
+else 
+{
+	echo '<div>BHAA Membership Page - We will check your membership status to know what to do</div>';
+}
+?>
 
 <?php get_footer(); ?>
