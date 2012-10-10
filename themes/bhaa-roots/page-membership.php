496fe4e31051cd97e4dbb6dfbd895ac8e7bcfaec
emeraldjava <paul.t.oconnell@gmail.com>
2012-10-10 18:43:44 +0100

    page-membership

---
+++ themes/bhaa-roots/page-membership.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * A custom BHAA membership page
+ * - will tell non registers to register
+ * - for registered users it displays the annual ticket.
+ */
+?>
+
+<?php get_header(); ?>
+BHAA Membership Page
+
+<?php get_footer(); ?>
