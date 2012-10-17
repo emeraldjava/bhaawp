1acccbd19bf74134fcd693a69c649237e73eec93
emeraldjava <paul.t.oconnell@gmail.com>
2012-10-17 19:44:27 +0100

    hide contact field on profile page.

--- themes/bhaatwentytwelve/functions.php
+++ themes/bhaatwentytwelve/functions.php
@@ -14,4 +14,29 @@ function bhaa_login_logo() {
 	</style>';
 }
 add_action('login_head', 'bhaa_login_logo');
+
+// http://wpmu.org/how-to-simplify-wordpress-profiles-by-removing-personal-options/
+function hide_personal_options(){
+	echo "\n" . '<script type="text/javascript">
+	jQuery(document).ready(function($)
+	{ 
+		$(\'form#your-profile > h3:first\').hide(); 
+		$(\'form#your-profile > table:first\').hide(); 
+		$(\'form#your-profile\').show(); 
+	});
+	</script>' . "\n";
+}
+add_action('admin_head','hide_personal_options');
+
+// http://codex.wordpress.org/Plugin_API/Filter_Reference#Author_and_User_Filters
+// http://wpmu.org/remove-aim-yahoo-and-jabber-fields-from-the-wordpress-profile-page/
+function edit_contactmethods( $contactmethods ) {
+	//$contactmethods['facebook'] = 'Facebook';
+	//$contactmethods['twitter'] = 'Twitter';
+	unset($contactmethods['yim']);
+	unset($contactmethods['aim']);
+	unset($contactmethods['jabber']);
+	return $contactmethods;
+}
+add_filter('user_contactmethods','edit_contactmethods',10,1);
 ?>
