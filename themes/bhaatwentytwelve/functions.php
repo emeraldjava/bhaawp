59acf88d0ca73502503ab0b783715203d806aa53
emeraldjava <paul.t.oconnell@gmail.com>
2012-10-17 19:11:23 +0100

    not nice - but

--- themes/bhaatwentytwelve/functions.php
+++ themes/bhaatwentytwelve/functions.php
@@ -5,7 +5,12 @@ remove_action('wp_head','wp_generator');
 // http://www.wprecipes.com/customize-wordpress-login-logo-without-a-plugin
 function bhaa_login_logo() {
 	echo '<style type="text/css">
-	h1 a { background-image:url('.get_template_directory_uri().'/images/logo.png) !important; }
+	h1 a { 
+	height:350px;
+	margin-left:10px;
+	margin-top:10px;
+	width:145px;
+	background-image:url('.content_url().'/themes/bhaatwentytwelve/images/logo.png) !important; }
 	</style>';
 }
 add_action('login_head', 'bhaa_login_logo');
