f6881a771fb2ddebac1971fce346b44c3317b908
emeraldjava <paul.t.oconnell@gmail.com>
2012-10-17 18:24:24 +0100

    template for the registration and runner result pages.

---
+++ themes/bhaatwentytwelve/page-runner-results.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ Template Name: BHAA Runner Results
+
+ http://www.expand2web.com/blog/custom-page-template-wordpress
+*/
+?>
+<?php get_header(); ?>
+BHAA Runner Page <?php echo $_REQUEST['id'];?>
+
+<?php 
+echo $loader->raceresult->getTable()->renderRunnerTable($_REQUEST['id']);
+?>
+
+<?php get_footer(); ?>
