7f22fd534f9394710469c054fcabc071532c0a9d
emeraldjava <paul.t.oconnell@gmail.com>
2012-10-19 17:07:22 +0100

    Rename the membership page to avoid a clash with register.

--- themes/bhaatwentytwelve/page-membership.php
+++ themes/bhaatwentytwelve/page-membership.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Template Name: BHAA Register
+ * Template Name: BHAA Membership
  * A custom BHAA membership page
  * - will tell non registers to register
  * - for registered users it displays the annual ticket.
- * 
  * 
  * plugins\events-manager\classes\em-events.php
  * 
@@ -13,6 +12,8 @@
 
 <?php get_header(); ?>
 
+<h1>BHAA Membership</h1>
+<p>This is the sales pitch to people.</p>
 <?php 
 if(!is_user_logged_in())
 {
@@ -31,7 +32,7 @@ else
 	echo('Welcome, ' . $current_user->display_name  . '</br>');
 	echo('Your membership status is : <b>' . $status  . '</b></br>');
 	
-	$EM_Event = new EM_Event(array('post_id'=>2052));
+	$EM_Event = new EM_Event(array('post_id'=>205712));// 205712 - 2052
 	//var_dump($EM_Event);
 	
 	// half works
