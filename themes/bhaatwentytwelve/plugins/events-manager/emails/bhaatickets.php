8cdac428744a3f61bb991138dfb6986a437c516f
emeraldjava <paul.t.oconnell@gmail.com>
2012-10-24 16:52:17 +0100

    some html style to the emails.

--- themes/bhaatwentytwelve/plugins/events-manager/emails/bhaatickets.php
+++ themes/bhaatwentytwelve/plugins/events-manager/emails/bhaatickets.php
@@ -2,38 +2,37 @@
 <head>
 <link rel='stylesheet' id='twentytwelve-style-css' href='<?php echo get_stylesheet_uri();?>' type='text/css' media='all'/>
 </head>
-<body>
-<div id="page">
+<body class="page">
+	<div id="page" class="hfeed site">
 
-<div id="header">
-<header id="masthead" class="site-header" role="banner">
-<hgroup>
-<?php 
-echo '<img class="bhaa-logo" src="'.get_stylesheet_directory_uri().'/images/logo.png" width="150" height="150" alt="BHAA Logo"/>';
-?>
-</hgroup>
-</header>
-</div>
+		<header id="masthead" class="site-header" role="banner">
+		<hgroup>
+		<?php 
+		echo '<img class="bhaa-logo" src="'.get_stylesheet_directory_uri().'/images/logo.png" width="150" height="150" alt="BHAA Logo"/>';
+		?>
+		</hgroup>
+		</header>
 
-<div id="main">
+	<div id="main" class="wrapper">
 <?php 
 foreach($EM_Booking->get_tickets_bookings() as $EM_Ticket_Booking): 
 if($EM_Ticket_Booking->get_ticket()->name=='Annual Membership')
 {
 ?>
-Hi #_BOOKINGNAME,
-
+<p>Hi #_BOOKINGNAME,</p>
+<pre>
 You are now a registered member for this year. Membership entitiles you to
 - reduced entry at races
 - run on a company team
 - take part in the leagues
+</pre>
+<p>Your Booking references is <b>#_BOOKINGID</b>.</p>
 
-Your Booking references is <b>#_BOOKINGID</b>
-
+<pre>
 Yours faithfully,
 #_CONTACTNAME
-
-Please email #_CONTACTEMAIL with any booking queries.
+</pre>
+<p>Please email #_CONTACTEMAIL with any booking queries.</p>
 <?php 
 }
 else 
@@ -64,7 +63,7 @@ Please email #_CONTACTEMAIL with any booking queries.
 }
 ?>
 <?php endforeach; ?>
-</div>
-</div>
+</div><!-- main -->
+</div><!-- page -->
 </body>
 </html>
