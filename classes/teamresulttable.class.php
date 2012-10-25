1a5ba4d1de8923954fa2f016fb0da5dc3ff62a13
emeraldjava <paul.t.oconnell@gmail.com>
2012-10-25 17:45:32 +0100

    Add the team name to the query.

--- classes/teamresulttable.class.php
+++ classes/teamresulttable.class.php
@@ -22,16 +22,11 @@ class TeamResultTable extends WP_List_Table
 		
 	function get_columns(){
 		$columns = array(
-			//'cb'        => '<input type="checkbox" />',
-			//'id' => 'ID',//<input type="checkbox" />', //Render a checkbox instead of text
-			'team'=>'Team',
-			//'race'=> 'Race',
-			//'league' => 'League',
+			'teamname'=>'Team',
 			'positiontotal'  => 'Position Total',
 			'standardtotal'  => 'Standard Total',
 			'leaguepoints' => 'League Points',
 			'class'  => 'Class',
-			//'status'=>'Status'
 		);
 		return $columns;
 	}
@@ -39,7 +34,7 @@ class TeamResultTable extends WP_List_Table
 	function column_default( $item, $column_name ) {
 		switch( $column_name )
 		{
- 			case 'team':
+ 			case 'teamname':
  			case 'class':
  			case 'positiontotal':
  			case 'standardtotal':
@@ -48,6 +43,15 @@ class TeamResultTable extends WP_List_Table
 			default:
 				return print_r( $item, true ) ; //Show the whole array for troubleshooting purposes
 		}
+	}
+	
+	function column_cname($item) {
+		return sprintf('<a href="/?post_type=company&p=%d">%s</a>',$item['cid'],$item['cname']);
+	}
+	
+	function column_display_name($item) {
+		$page = get_page_by_title('member');
+		return sprintf('<a href="/?page_id=%d&id=%d">%s</a>',$page->ID,$item['runner'],$item['display_name']);
 	}
 	
 	function prepare_items($race)
@@ -64,16 +68,13 @@ class TeamResultTable extends WP_List_Table
 // 			join wp_users on wp_users.id=wp_bhaa_raceresult.runner';
 // 		else
 		$query = '
-			SELECT '.$wpdb->prefix .'bhaa_teamresult.*
+			SELECT '.$wpdb->prefix .'bhaa_teamresult.*,wp_posts.post_title as teamname
 			FROM '.$wpdb->prefix .'bhaa_teamresult
+			left join wp_posts on wp_posts.post_type="house" and '.$wpdb->prefix .'bhaa_teamresult.team=wp_posts.id
 			where race=201219 order by class, positiontotal';
 		//	join wp_posts on wp_posts.id=wp_bhaa_raceresult
 		echo '<p>'.$query.'</p>';
-		//error_log($mgs);
-	
-		//echo $query;
 		$totalitems = $wpdb->query($query);
-		//echo $totalitems;
 		$this->items = $wpdb->get_results($query,ARRAY_A);
 	}
 	
