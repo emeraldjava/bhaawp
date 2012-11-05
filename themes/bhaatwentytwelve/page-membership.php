fd1d0337707619e8a9933296cb2db54ddef65770
emeraldjava <paul.t.oconnell@gmail.com>
2012-11-05 13:37:42 +0000

    update the membership page.

--- themes/bhaatwentytwelve/page-membership.php
+++ themes/bhaatwentytwelve/page-membership.php
@@ -15,19 +15,20 @@
 <div id="template-front-page" class="template-front-page">
 <div>
 <h1>BHAA Membership</h1>
-<p>This is the sales pitch to people.</p>
-<p>Currently implemented as a Page Template.</p>
-<ul>
+<div>
+<ol id="reasonstojoin">
 <li>Reduced membership to all races</li>
-<li>Run as a member of a team</li>
-<li>Races count towards the leagues</li>
-</ul>
+<li>Run as a member of a team in prizes</li>
+<li>Be counted in the summer and winter leagues</li>
+<li>Easier registration on the day</li>
+</ol>
+</div>
 </div>
 <div id="right-sidebar" class="right-sidebar">
 <?php 
 if(!is_user_logged_in())
 {
-	echo '<p>Outline benefits here. Tell them to register and pay via the user profile.</p>';
+	echo '<p>We first need you to register your details onlines.</p>';
 	echo '<p>BHAA Membership Page - Please use the <a href="./register">registration form</a> to create a user account.</p>';
 }
 else 
@@ -36,31 +37,34 @@ else
 	get_currentuserinfo();
 	
 	$status = get_user_meta(get_current_user_id(),'bhaa_runner_status',true);
+	$dateofrenewal = get_user_meta(get_current_user_id(),'bhaa_runner_dateofrenewal',true);
 	
 	echo '<div>BHAA Membership Page</div>';
-	echo('We will check your membership status to know what to do');
+	//echo('We will check your membership status to know what to do');
 	echo('Welcome, ' . $current_user->display_name  . '</br>');
-	echo('Your membership status is : <b>' . $status  . '</b></br>');
+	echo('Your membership status is : <b>' . $status  . '</b>. Your last renewal date was '.$dateofrenewal.'</br>');
 	
+	/**
+	 * 2052 - wpdemo
+	 * 205712 - ae local
+	 */
 	$EM_Event = new EM_Event(array('post_id'=>205712));// 205712 - 2052
-	var_dump($EM_Event);
-	
-	// half works
-	//echo $EM_Event->output_single();
-	echo $EM_Event->output('<div id="annualmembershup">
-	{has_bookings}
-			#_BOOKINGFORM
-			{/has_bookings}
-			</div>');
 			
 	if(isset($status) && $status==("I"))
 	{
 		//echo('Please renew - via this shortcode hack.</br>');
-		//echo do_shortcode('[bhaaraceday]');
+		// half works
+		//echo $EM_Event->output_single();
+		echo $EM_Event->output(
+		'<div id="annualmembershup">
+		{has_bookings}
+			#_BOOKINGFORM
+		{/has_bookings}
+		</div>');
 	}
 	else
 	{
-		//echo('You are a current member of the BHAA.');
+		echo('Membership renewal can be done again next year.');
 	}
 }
 ?>
