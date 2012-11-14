6e5ba95c87b0cca64a423587554611f45b4dd53a
emeraldjava <paul.t.oconnell@gmail.com>
2012-11-14 20:07:52 +0000

    remove index warning issue.

--- themes/bhaatwentytwelve/plugins/events-manager/templates/event-single.php
+++ themes/bhaatwentytwelve/plugins/events-manager/templates/event-single.php
@@ -131,17 +131,15 @@ else
 }
 
 
-$custom = get_post_custom(the_ID());//$post->ID);
+$custom = get_post_custom(the_ID());
 echo '<div id="media"><h3>Media</h3></div></div>';
-$youtube = $custom["youtube"][0];
+$youtube = $custom['youtube'];
 if (isset($youtube)) 
 	echo "<p><iframe title='YouTube video player' width='400' height='275' src='http://www.youtube.com/embed/" . $youtube . "' frameborder='0' allowfullscreen></iframe></p>";
 
-$flickr = $custom["flickr"][0];
+$flickr = $custom['flickr'];
 if (isset($flickr))
 	echo "FLICKR";
-//if ($vimeo != NULL) echo "<p><iframe src='http://player.vimeo.com/video/" . $vimeo . "?portrait=0' width='400' height='275' frameborder='0'></iframe></p>";
-//echo '</div>';
 
 echo '</section>';
 //get_sidebar();
