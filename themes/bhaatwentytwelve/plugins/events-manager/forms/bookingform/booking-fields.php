a7aa6b4f678b8e53e2cbee08110f341c98a73bd1
Paul O'Connell <paul.t.oconnell@gmail.com>
2012-11-20 21:39:10 +0000

    drop two fields

--- themes/bhaatwentytwelve/plugins/events-manager/forms/bookingform/booking-fields.php
+++ themes/bhaatwentytwelve/plugins/events-manager/forms/bookingform/booking-fields.php
@@ -8,14 +8,6 @@
 <?php if( !is_user_logged_in() && apply_filters('em_booking_form_show_register_form',true) ): ?>
 	<?php //User can book an event without registering, a username will be created for them based on their email and a random password will be created. ?>
 	<input type="hidden" name="register_user" value="1" />
-	<p>
-		<label for='user_name'><?php _e('Username','dbem') ?></label>
-		<input type="text" name="user_name" id="user_name" class="input" <?php if(!empty($_REQUEST['user_name'])) echo "value='{$_REQUEST['user_name']}'"; ?> />
-	</p>
-	<p>
-		<label for='user_email'><?php _e('E-mail','dbem') ?></label> 
-		<input type="text" name="user_email" id="user_email" class="input" <?php if(!empty($_REQUEST['user_email'])) echo "value='{$_REQUEST['user_email']}'"; ?>  />
-	</p>
 	<?php 
 	echo do_shortcode('[user-meta type="both" form="registration"]');
 	//do_action('register_form'); //careful if making an add-on, this will only be used if you're not using custom booking forms ?>					
