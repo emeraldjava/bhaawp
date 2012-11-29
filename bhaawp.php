05ddafbb9677c90e6504823bb8a2e48ed933dd4e
emeraldjava <paul.t.oconnell@gmail.com>
2012-11-29 18:04:30 +0000

    update date

--- bhaawp.php
+++ bhaawp.php
@@ -3,14 +3,14 @@
 Plugin Name: BHAA Plugin
 Plugin URI: https://github.com/emeraldjava/bhaawp
 Description: Plugin to handle bhaa results
-Version: 2012.11.20
+Version: 2012.11.29
 Author: paul.t.oconnell@gmail.com
 Author URI: https://github.com/emeraldjava/bhaawp
 */
 
 class BhaaLoader
 {
-	var $version = '2012.11.20';
+	var $version = '2012.11.29';
 	
 	var $admin;
 	var $connection;
