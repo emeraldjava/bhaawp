943e5881bd8b86854124031cef66fb60ea545b31
emeraldjava <paul.t.oconnell@gmail.com>
2012-12-07 15:28:44 +0000

    use the user slug as the main parameter.

--- themes/Avada/page-runner.php
+++ themes/Avada/page-runner.php
@@ -3,12 +3,23 @@
  Template Name: BHAA Runner
 */
 ?>
-<?php get_header(); ?>
-BHAA Runner Page
+<?php get_header();?>
 
 <?php 
 global $loader;
-echo $loader->raceresult->getTable()->renderRunnerTable($_REQUEST['id']);
-?>
+//echo 'BHAA Runner Page : Name = '.$_REQUEST['user_nicename'];
+$user = get_user_by('slug', $_REQUEST['user_nicename']);
+//var_dump($user);
+// $user = get_user_by('nicename', $_REQUEST['user_nicename']);
+// get_userd
+// $user_nicename = $_REQUEST['name'];
+// echo 'BHAA Runner Page : Name = '.$_REQUEST['name'];
+// echo 'BHAA Runner Page : Name = '.$_GET['name'];
+// //if (isset($wp_query->query_vars['name']))
+// //{
+// 	echo 'query var '.$wp_query->query_vars['name'];
+// 	echo 'query var '.get_query_var( 'name' );
+// //}
 
-<?php get_footer(); ?>
+echo $loader->raceresult->getTable()->renderRunnerTable($user->ID);//$_REQUEST['id']);
+?>
