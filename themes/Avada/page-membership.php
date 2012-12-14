f72b7495c967231e8e763cb2029e501e71c35f47
emeraldjava <paul.t.oconnell@gmail.com>
2012-12-14 15:14:36 +0000

    updated membership page.

--- themes/Avada/page-membership.php
+++ themes/Avada/page-membership.php
@@ -4,17 +4,10 @@
  * A custom BHAA membership page
  * - will tell non registers to register
  * - for registered users it displays the annual ticket.
- * 
- * plugins\events-manager\classes\em-events.php
- * 
  */
 ?>
 
-<?php get_header(); 
-/**
- * The sales pitch - should be a static page
- */
-?>
+<?php get_header(); ?>
 <div id="template-front-page" class="template-front-page">
 <?php
 // query for the membership page for its content
@@ -30,9 +23,20 @@ wp_reset_postdata();
 /**
  * check the status of the runner - determine options
  */
+/**
+ * display the annual booking ticket and registration form.
+ */
+$bhaa_annual_event_id = get_option( 'bhaa_annual_event_id',0);
+$event = em_get_event($bhaa_annual_event_id,'post_id');
 if(!is_user_logged_in())
 {
 	//echo '<p>Your not LOGGED in.</p>';
+	echo $event->output(
+	'<div id="annualmembership">
+	{has_bookings}
+		#_BOOKINGFORM
+	{/has_bookings}
+	</div>');
 }
 else 
 {
@@ -43,21 +47,19 @@ else
 	$status = get_user_meta(get_current_user_id(),'bhaa_runner_status',true);
 	$dateofrenewal = get_user_meta(get_current_user_id(),'bhaa_runner_dateofrenewal',true);
 	
-	//echo '<div>BHAA Membership Page</div>';
-	//echo('We will check your membership status to know what to do');
-	//echo('Welcome, ' . $current_user->display_name  . '</br>');
-	//echo('Your membership status is : <b>' . $status  . '</b>. Your last renewal date was '.$dateofrenewal.'</br>');
+	if($status=="M")
+	{
+		echo('Hi ' . $current_user->display_name  . ',</br>');
+		echo('You are a current BHAA member and do not need to renew your membership yet.');
+	}
+	else
+	{
+		echo $event->output(
+			'<div id="annualmembership">
+			{has_bookings}
+			#_BOOKINGFORM
+			{/has_bookings}
+			</div>');
+	}
 }
-
-/**
- * display the annual booking ticket and registration form.
- */
-$bhaa_annual_event_id = get_option( 'bhaa_annual_event_id',0);
-$event = em_get_event($bhaa_annual_event_id,'post_id');
-echo $event->output(
-	'<div id="annualmembership">
-	{has_bookings}
-		#_BOOKINGFORM
-	{/has_bookings}
-	</div>');
 ?>
