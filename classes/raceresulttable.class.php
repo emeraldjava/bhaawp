e43d2e928f573b8e0236bb492318c32428e8d0db
emeraldjava <paul.t.oconnell@gmail.com>
2012-12-19 18:45:45 +0000

    import the correct template file.

--- classes/raceresulttable.class.php
+++ classes/raceresulttable.class.php
@@ -1,6 +1,6 @@
 <?php
 if(!class_exists('WP_List_Table')){
-	require_once( ABSPATH . 'wp-admin/includes/screen.php' );
+	require_once( ABSPATH . 'wp-admin/includes/template.php' );
 	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
 
