6d60f7dcc131e8f7b067034c97cc9c576759a3b9
emeraldjava <paul.t.oconnell@gmail.com>
2013-01-17 18:12:17 +0000

    Add getKmPace()

--- classes/standard.class.php
+++ classes/standard.class.php
@@ -18,17 +18,40 @@ class Standard
 		return 'Standard '.$this->standard;
 	}
 	
-	function getTime($kmDistance)
+	/**
+	 * Calculates the expected time for a specific distance for this standard
+	 * -- SEC_TO_TIME((((standard.slopefactor) * (sd.km-1)) + oneKmTimeInSecs) * sd.km) as time,
+	 * @param unknown $kmDistance
+	 * @return string
+	 */
+	function getKmTime($kmDistance)
 	{
-		// SEC_TO_TIME((((standard.slopefactor) * (sd.km-1)) + oneKmTimeInSecs) ) as pace,
-		// SEC_TO_TIME((((standard.slopefactor) * (sd.km-1)) + oneKmTimeInSecs) * sd.km) as time,
-		return date('i:s', (($this->slopefactor * ($kmDistance-1)) + $this->oneKmTimeInSecs) * $kmDistance ).'/n';
+		$seconds = (($this->slopefactor * ($kmDistance-1)) + $this->oneKmTimeInSecs) * $kmDistance;
+		if($seconds >= 3600)
+			return gmdate('H:i:s',$seconds);
+		else
+			return gmdate('i:s',$seconds);
+	}
+	
+	/**
+	 * Returns the required pace for a specific distance
+	 * -- SEC_TO_TIME((((standard.slopefactor) * (sd.km-1)) + oneKmTimeInSecs) ) as pace,
+	 */
+	function getKmPace($kmDistance)
+	{
+		$seconds = (($this->slopefactor * ($kmDistance-1)) + $this->oneKmTimeInSecs);
+		return gmdate('i:s',$seconds);
 	}
 }
 $ONE = new Standard(1, 0.442101708254709, 176.435763853992, 174.688875102962);
-echo $ONE->toString();
-echo $ONE->getTime(1);
-echo $ONE->getTime(2);
-echo $ONE->getTime(5);
-echo $ONE->getTime(42);
+echo $ONE->toString().',';
+echo $ONE->getKmTime(1).',';
+echo $ONE->getKmTime(2).',';
+echo $ONE->getKmTime(5).',';
+echo $ONE->getKmTime(42).',';
+
+echo $ONE->getKmPace(1).',';
+echo $ONE->getKmPace(2).',';
+echo $ONE->getKmPace(5).',';
+echo $ONE->getKmPace(42).',';
 ?>
