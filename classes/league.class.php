5ff1b2134b51d70a4d01ae27d56aeae3abb03e21
emeraldjava <paul.t.oconnell@gmail.com>
2013-01-22 16:47:30 +0000

    league logic.

--- classes/league.class.php
+++ classes/league.class.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * 
- * http://themergency.com/generators/wordpress-custom-post-types
- * 
- * http://codex.wordpress.org/Function_Reference/register_taxonomy
- * http://codex.wordpress.org/Function_Reference/wp_insert_term
- * 
- * http://wp.tutsplus.com/tutorials/theme-development/innovative-uses-of-wordpress-post-types-and-taxonomies/
- * 
+ * This class handle logic of the BHAA leagues.
  * @author oconnellp
  *
  */
 class League
 {
-
+	function __construct()
+	{	
+	}
+	
+	function getEvents()
+	{
+		return "TODO";	
+	}
 }
 ?>
