e831f4163da9675d0afdb0f27fb6709cd8e7f634
emeraldjava <paul.t.oconnell@gmail.com>
2013-01-29 15:55:09 +0000

    simple email template.

--- classes/runner.class.php
+++ classes/runner.class.php
@@ -128,21 +128,22 @@ class Runner
 			$action = $_GET['action'];
 			$user = get_userdata($user_id);
 			
-// 			if($user->user_email!=''||$user->user_email!=null)
-// 			{
-// 				$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
-// 				$message  = sprintf(__('Dear %s:'), $user->user_firstname) . "\n";
-// 				$message  = sprintf(__('Your BHAA Membership has been renewed %s:'), $blogname) . "\n";
-// 				//$message .= sprintf(__('Username: %s'), stripslashes($user->user_login)) . "\r\n\r\n";
-// 				//$message .= sprintf(__('E-mail: %s'), stripslashes($user->user_email)) . "\r\n";
-// 				//Prepare headers for HTML
-// 				$headers  = 'MIME-Version: 1.0' . "\r\n";
-// 				$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-// 				$headers .= 'From: Business Houses Athletic Association <info@bhaa.ie>' . "\r\n";
+ 			if($user->user_email!=''||$user->user_email!=null)
+ 			{
+ 				$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+ 				$message  = sprintf(__('Dear %s:'), $user->user_firstname) . "\n";
+ 				$message  = sprintf(__('Your BHAA Membership has been renewed %s:'), $blogname) . "\n";
+				$message  = sprintf(__('You have to option to leave comments and see / manage your own contact details when you login into the website')) . "\n";
+ 				//$message .= sprintf(__('Username: %s'), stripslashes($user->user_login)) . "\r\n\r\n";
+ 				//$message .= sprintf(__('E-mail: %s'), stripslashes($user->user_email)) . "\r\n";
+ 				//Prepare headers for HTML
+ 				$headers  = 'MIME-Version: 1.0' . "\r\n";
+ 				$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+ 				$headers .= 'From: Business Houses Athletic Association <info@bhaa.ie>' . "\r\n";
 				
-// 				$res = wp_mail($user->user_email, 'BHAA Renewal : '.$user->user_firstname." ".$user->user_lastname , $message, $headers);
-// 				error_log('email sent ? '.$res);
-// 			}
+ 				$res = wp_mail($user->user_email, 'BHAA Renewal : '.$user->user_firstname." ".$user->user_lastname , $message, $headers);
+ 				error_log('email sent ? '.$res);
+ 			}
 
 			update_user_meta($user_id, Runner::BHAA_RUNNER_STATUS, 'M');
 			update_user_meta($user_id, Runner::BHAA_RUNNER_DATEOFRENEWAL,date('Y-m-d'));
