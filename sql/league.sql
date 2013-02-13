9832bb54b2201c7cbc7589bd3e8745322131f82f
emeraldjava <paul.t.oconnell@gmail.com>
2013-02-13 18:17:29 +0000

    league sql

---
+++ sql/league.sql
@@ -0,0 +1,19 @@
+
+INSERT INTO wp_bhaa_import (id, tag, type, new, old) VALUES
+(NULL, 'winter2013', 'league', 2492, 13);
+
+select * from wp_posts where post_type='league';
+
+select * from bhaaie_members.league
+
+
+select * from wp_bhaa_raceresult where runner=7713;
+
+select * from wp_bhaa_raceresult where runner=7713;
+
+select * from bhaaie_members.racepointsdata where runner=7713;
+
+select * from bhaaie_members.racepoints where runner=7713;
+
+select * from bhaaie_members.leaguesummary where leagueid=13;
+
