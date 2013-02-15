ce6c3802d967a08580a9e2d02df68afda21d1e47
emeraldjava <paul.t.oconnell@gmail.com>
2013-02-15 18:11:59 +0000

    league stuff

--- sql/league.sql
+++ sql/league.sql
@@ -16,8 +16,9 @@ select * from bhaaie_members.racepointsdata where runner=7713;
 select * from bhaaie_members.racepoints where runner=7713;
 
 select * from bhaaie_members.leaguesummary where leagueid=13;
+update wp_bhaa_leaguesummary set league=2492 where league=13;
 
 INSERT INTO wp_bhaa_leaguesummary(league,leaguetype,leagueparticipant,leaguestandard,leaguedivision,leagueposition,leaguescorecount,leaguepoints)
 select
-13,"I",leagueparticipantid,leaguestandard,leaguedivision,leagueposition,leaguescorecount,leaguepoints
+2492,"I",leagueparticipantid,leaguestandard,leaguedivision,leagueposition,leaguescorecount,leaguepoints
 from bhaaie_members.leaguesummary where leagueid=13;
