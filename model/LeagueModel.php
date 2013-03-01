e13308b6a20a0d0dcfe294ea40f7b2ad1bc78fc2
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-01 15:46:39 +0000

    use the WP_Query to get league / event details

--- model/LeagueModel.php
+++ model/LeagueModel.php
@@ -9,19 +9,32 @@ class LeagueModel extends BaseModel
 		$this->leagueid = $leagueid;
 	}
 	
+	
 	function getEvents()
 	{
 		$events = p2p_get_connections(Connection::LEAGUE_TO_EVENT,
 			array('direction'=>'from','from'=>$this->leagueid,'fields'=>'p2p_to')
 		);
-		echo 'races'.print_r($events);
+		//echo 'races'.print_r($events);
+
+		$x = new WP_Query(array(
+				'connected_type' => 'league_to_event',
+				//'post_type' => 'league',connected_items
+				'connected_items' => $this->leagueid,//'any',
+				'nopaging' => true,
+			)
+		);
+		echo $x->request;
+		echo $x->get_posts();
+		
+		return print_r($x->get_posts(),true);
 		// 	$res = array();
 		// 	foreach($races as $raceid) {
 		// 		$race = new RaceModel($raceid);
 		// 		$res[] = $race;
 		// 	}
 		//echo 'x'.$res;
-		return print_r($events);
+		//return $events;
 	}
 }
 ?>
