4c8e1c07f43036083a84c72d38c6b2317362365e
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-05 18:59:21 +0000

    get_results

--- model/LeagueModel.php
+++ model/LeagueModel.php
@@ -9,19 +9,43 @@ class LeagueModel extends BaseModel
 		$this->leagueid = $leagueid;
 	}
 	
-	public function getLeagueTagsAndRaces($league,$gender)
+	function getLeagueRaces($type='')
 	{
-		$select = $this->select()
-		->setIntegrityCheck(false)
-		->from(array('leagueevent'=>'leagueevent'),
-				array('(CASE leagueevent.summaryrace WHEN 0 THEN race.id ELSE leagueevent.summaryrace END) as raceid'))
-				->join(array('event'=>'event'),'event.id = leagueevent.event',array('tag'))
-				->join(array('race'=>'race'),'event.id = race.event',array())
-				->where('leagueevent.league = ?',$league)
-				->where("race.type in ('C','S',?)",$gender)
-				->group('raceid')
-				->order('event.date ASC');
-		return $this->fetchAll($select);
+		$SQL = $this->wpdb->prepare("select l.ID as lid,l.post_title,
+			e.ID as eid,e.post_title as etitle,
+			r.ID as rid,r.post_title as rtitle,r_type.meta_value as rtype 
+			from wp_posts l
+			inner join wp_p2p l2e on (l2e.p2p_type='league_to_event' and l2e.p2p_from=l.ID)
+			inner join wp_posts e on (e.id=l2e.p2p_to)
+			inner join wp_p2p e2r on (e2r.p2p_type='event_to_race' and e2r.p2p_from=e.ID)
+			inner join wp_posts r on (r.id=e2r.p2p_to)
+			inner join wp_postmeta r_type on (r_type.post_id=r.id and r_type.meta_key='bhaa_race_type')
+			where l.post_type='league'
+			and l.ID=%d", $this->leagueid);
+		if($type!='')
+			$SQL .= sprintf("and r_type.meta_value in ('C','%s')",$type);
+		echo $SQL;
+		//$this->wpdb->query($SQL);
+
+		// OBJECT, OBJECT_K, ARRAY_A, ARRAY_N
+		return $this->wpdb->get_results($SQL,OBJECT);
+		
+// 		$select = $this->select()
+// 		->setIntegrityCheck(false)
+// 		->from(array('leagueevent'=>'leagueevent'),
+// 				array('(CASE leagueevent.summaryrace WHEN 0 THEN race.id ELSE leagueevent.summaryrace END) as raceid'))
+// 				->join(array('event'=>'event'),'event.id = leagueevent.event',array('tag'))
+// 				->join(array('race'=>'race'),'event.id = race.event',array())
+// 				->where('leagueevent.league = ?',$league)
+// 				->where("race.type in ('C','S',?)",$gender)
+// 				->group('raceid')
+// 				->order('event.date ASC');
+// 		return $this->fetchAll($select);
+	}
+	
+	function getRunnerLeagueSummary($runner)
+	{
+		return $runner;
 	}
 	
 	/**
