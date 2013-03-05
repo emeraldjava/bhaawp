7b1cc542f040ebcbd4b66b394b70d60a089be500
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-05 17:41:59 +0000

    getLeagueTagsAndRaces

--- model/LeagueModel.php
+++ model/LeagueModel.php
@@ -9,6 +9,20 @@ class LeagueModel extends BaseModel
 		$this->leagueid = $leagueid;
 	}
 	
+	public function getLeagueTagsAndRaces($league,$gender)
+	{
+		$select = $this->select()
+		->setIntegrityCheck(false)
+		->from(array('leagueevent'=>'leagueevent'),
+				array('(CASE leagueevent.summaryrace WHEN 0 THEN race.id ELSE leagueevent.summaryrace END) as raceid'))
+				->join(array('event'=>'event'),'event.id = leagueevent.event',array('tag'))
+				->join(array('race'=>'race'),'event.id = race.event',array())
+				->where('leagueevent.league = ?',$league)
+				->where("race.type in ('C','S',?)",$gender)
+				->group('raceid')
+				->order('event.date ASC');
+		return $this->fetchAll($select);
+	}
 	
 	/**
 	 * http://zanematthew.com/blog/wordpress-advanced-meta-query-using-wp_query/
