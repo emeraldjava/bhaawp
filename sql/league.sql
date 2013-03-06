c7f349631d2d19df9d891c2be26a07d14d3167e9
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-06 12:39:55 +0000

    league query

--- sql/league.sql
+++ sql/league.sql
@@ -7,7 +7,7 @@ select * from wp_posts where post_type='event';
 
 select l.ID as lid,l.post_title,
 e.ID as eid,e.post_title as etitle,
-r.ID as rid,r.post_title as rtitle,r_type.meta_value as type 
+r.ID as rid,r.post_title as rtitle,r_type.meta_value as rtype 
 from wp_posts l
 inner join wp_p2p l2e on (l2e.p2p_type='league_to_event' and l2e.p2p_from=l.ID)
 inner join wp_posts e on (e.id=l2e.p2p_to)
@@ -16,6 +16,7 @@ inner join wp_posts r on (r.id=e2r.p2p_to)
 inner join wp_postmeta r_type on (r_type.post_id=r.id and r_type.meta_key='bhaa_race_type')
 where l.post_type='league'
 and l.ID=2492;
+and r_type.meta_value in ('C','S','*')
 
 --inner join wp_postmeta r_type on (r_type.post_id=r.id and r_type.meta_key='bhaa_race_type')
 
