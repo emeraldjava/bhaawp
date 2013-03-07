a3d5f4558a8c3e111882835f38d624676b5e75b7
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-07 12:32:53 +0000

    Add event columns

--- model/LeagueModel.php
+++ model/LeagueModel.php
@@ -9,27 +9,6 @@ class LeagueModel extends BaseModel
 		$this->leagueid = $leagueid;
 	}
 	
-	function getLeagueRaces($type='')
-	{
-		$SQL = $this->wpdb->prepare("select l.ID as lid,l.post_title,
-			e.ID as eid,e.post_title as etitle,
-			r.ID as rid,r.post_title as rtitle,r_type.meta_value as rtype 
-			from wp_posts l
-			inner join wp_p2p l2e on (l2e.p2p_type='league_to_event' and l2e.p2p_from=l.ID)
-			inner join wp_posts e on (e.id=l2e.p2p_to)
-			inner join wp_p2p e2r on (e2r.p2p_type='event_to_race' and e2r.p2p_from=e.ID)
-			inner join wp_posts r on (r.id=e2r.p2p_to)
-			inner join wp_postmeta r_type on (r_type.post_id=r.id and r_type.meta_key='bhaa_race_type')
-			where l.post_type='league'
-			and l.ID=%d", $this->leagueid);
-		if($type!='')
-			$SQL .= sprintf(" and r_type.meta_value in ('C','%s')",$type);
-		echo $SQL;
-		error_log($SQL);
-		// OBJECT, OBJECT_K, ARRAY_A, ARRAY_N
-		return $this->wpdb->get_results($SQL,OBJECT);
-	}
-	
 	function getRunnerLeagueSummary($races,$runner)
 	{
 		// $this->wpdb->prepare
