af309862c805d6a08721864a77e2db0b4cd80dfd
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-03-08 22:52:51 +0000

    teacher race ids

--- model/LeagueSummary.php
+++ model/LeagueSummary.php
@@ -110,8 +110,26 @@ class LeagueSummary extends BaseModel implements Table
 	function updateLeague($division='L1')
 	{
 		// get all league races
+<<<<<<< HEAD
 		$mens_races = $this->getRaceIdSetString($this->getLeagueRaces('M'));
 		$womens_races = $this->getRaceIdSetString($this->getLeagueRaces('W'));
+=======
+		$races = $this->getLeagueRaces('M');
+// 		$rid_array = array_map(
+// 			function($val) {
+// 				return $val->rid;
+// 			},
+// 			$races);
+		$rid_array = array(1784);
+		$mens_races = implode(",", $rid_array);
+		
+		$wraces = $this->getLeagueRaces('W');
+		//$wrid_array = array_map(function($val) {  return $val->rid;}, $wraces);
+		$wrid_array = array(1783);
+		$womens_races = implode(",", $wrid_array);
+		//$mens_races = implode(",", $race_set );// array_map( function($val) { return $val->rid; } , $this->getLeagueRaces('M') ) );
+		//$womens_races = implode(",", array_map( function($val) { return $val->rid; } , $this->getLeagueRaces('W')));
+>>>>>>> teacher race ids
 		
 		// for each of the runners - update the league details
 		$runners = $this->wpdb->get_results(
