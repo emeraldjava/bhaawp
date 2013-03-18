22a2a8b46e130ce5426150a30c9504e3723bcf7c
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-03-18 21:25:14 +0000

    update status and dor, select new wp users

--- sql/wp_users.sql
+++ sql/wp_users.sql
@@ -27,8 +27,23 @@ join wp_usermeta dor on (dor.user_id=u.id AND dor.meta_key = 'bhaa_runner_dateof
 where YEAR(dor.meta_value)=2013 order by dor.meta_value desc;
 
 -- select wp renewed but not upated in members
-select wp_users.id,status.meta_value,dor.meta_value,runner.id,runner.status,runner.dateofrenewal from wp_users
+select wp_users.user_nicename,wp_users.id,status.meta_value,dor.meta_value,runner.id,runner.status,runner.dateofrenewal from wp_users
 join wp_usermeta status on (status.user_id=wp_users.id and status.meta_key='bhaa_runner_status' and status.meta_value='M')
 join wp_usermeta dor on (dor.user_id=wp_users.id and dor.meta_key='bhaa_runner_dateofrenewal')
 join bhaaie_members.runner runner on runner.id=wp_users.id
 where runner.status!='M' and YEAR(dor.meta_value)=2013;
+
+update wp_users
+join wp_usermeta status on (status.user_id=wp_users.id and status.meta_key='bhaa_runner_status' and status.meta_value='M')
+join wp_usermeta dor on (dor.user_id=wp_users.id and dor.meta_key='bhaa_runner_dateofrenewal')
+join bhaaie_members.runner runner on runner.id=wp_users.id
+set
+runner.status=status.meta_value,
+runner.dateofrenewal=dor.meta_value
+where runner.status!='M' and YEAR(dor.meta_value)=2013;
+
+-- handle new wp user not in the members db
+select user_nicename,id,user_registered from wp_users where YEAR(user_registered)=2013;
+
+
+
