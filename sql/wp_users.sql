395519b2c49f1ba6e5cdc0cf7f0423e40f2ed838
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-03-18 21:33:03 +0000

    insert wp users to members runners table

--- sql/wp_users.sql
+++ sql/wp_users.sql
@@ -45,5 +45,36 @@ where runner.status!='M' and YEAR(dor.meta_value)=2013;
 -- handle new wp user not in the members db
 select user_nicename,id,user_registered from wp_users where YEAR(user_registered)=2013;
 
+-- list the two max id's
+select MAX(id) from wp_users;
+select MAX(id) from bhaaie_members.runner;
 
+INSERT INTO bhaaie_members.runner(id,firstname,surname,gender,dateofbirth,company,email,mobilephone,status,insertdate,dateofrenewal)
+select wp_users.id,mfn.meta_value,mln.meta_value,mg.meta_value,mdob.meta_value,mc.meta_value,wp_users.user_email,mp.meta_value,ms.meta_value,mdor.meta_value,mdor.meta_value from wp_users
+join wp_usermeta ms on (
+	ms.user_id=wp_users.id and 
+	ms.meta_key='bhaa_runner_status')
+join wp_usermeta mfn on (
+	mfn.user_id=wp_users.id and 
+	mfn.meta_key='first_name') 
+join wp_usermeta mln on (
+	mln.user_id=wp_users.id and 
+	mln.meta_key='last_name') 
+join wp_usermeta mg on (
+	mg.user_id=wp_users.id and 
+	mg.meta_key='bhaa_runner_gender') 
+join wp_usermeta mdob on (
+	mdob.user_id=wp_users.id and 
+	mdob.meta_key='bhaa_runner_dateofbirth') 
+left join wp_usermeta mc on (
+	mc.user_id=wp_users.id and 
+	mc.meta_key='bhaa_runner_company') 
+join wp_usermeta mp on (
+	mp.user_id=wp_users.id and 
+	mp.meta_key='bhaa_runner_mobilephone') 
+join wp_usermeta mdor on (
+	mdor.user_id=wp_users.id and 
+	mdor.meta_key='bhaa_runner_dateofrenewal') 	
+where id>=22965
+order by wp_users.id;
 
