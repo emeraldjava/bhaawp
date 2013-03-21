1b0b86b96bdd9d4fcd59c60b9c730bf7870bff7f
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-21 11:26:15 +0000

    wp_bhaa_houses

---
+++ sql/wp_bhaa_houses.sql
@@ -0,0 +1,8 @@
+
+
+-- rename the teamtype values
+select * from wp_term_taxonomy where taxonomy = 'teamtype'
+select * from wp_terms where term_id in (88,89,90)
+update wp_terms set name='company',slug='company' where name='companyteam';
+update wp_terms set name='sector',slug='sector' where name='sectorteam';
+update wp_terms set name='inactive',slug='inactive' where name='inactiveteam';
