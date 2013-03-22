89c008337dbfc32b14000e9ac4c382515f3cc191
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-22 17:23:34 +0000

    disvision sql

--- sql/wp_bhaa_division.sql
+++ sql/wp_bhaa_division.sql
@@ -28,3 +28,31 @@ INSERT INTO `wp_bhaa_division` (`id`, `name`, `code`, `gender`, `min`, `max`, `t
 (11, 'Mens Team League C', 'C', 'M', 39, 46, 'T'),
 (12, 'Mens Team League D', 'D', 'M', 47, 90, 'T'),
 (13, 'Womens Team League', 'W', 'W', 1, 90, 'T');
+
+select * from wp_bhaa_division where type="I";
+
+select code from wp_bhaa_division 
+where 7 between min and max and gender="W" and type="I" 
+
+select code,gender.meta_value from wp_bhaa_division 
+JOIN wp_usermeta gender ON (gender.user_id=id AND gender.meta_key = 'bhaa_runner_gender')
+where 7 between min and max and gender="W" and type="I" and id=7713
+
+select code,gender.meta_value,standard.meta_value from wp_bhaa_division 
+JOIN wp_usermeta gender ON (gender.user_id=id AND gender.meta_key = 'bhaa_runner_gender')
+JOIN wp_usermeta standard ON (standard.user_id=id AND standard.meta_key = 'bhaa_runner_standard')
+where standard.meta_value between min and max and gender=gender.meta_value and type="I" and id=7713
+
+select code,gender.meta_value,standard.meta_value from wp_bhaa_division
+inner JOIN wp_usermeta gender ON (gender.user_id=id AND gender.meta_key = 'bhaa_runner_gender')
+inner JOIN wp_usermeta standard ON (standard.user_id=id AND standard.meta_key = 'bhaa_runner_standard')
+where standard.meta_value between min and max and gender=gender.meta_value and type="I" and id=7713
+
+select lrd.runner, d.code, lrd.pointsTotal
+  from leaguerunnerdata lrd
+  join runner r on lrd.runner = r.id
+  join division d on lrd.standard between d.min and d.max and d.type='I' and 
+d.gender=r.gender
+  where  league=6 and runner = 7713
+  
+  
