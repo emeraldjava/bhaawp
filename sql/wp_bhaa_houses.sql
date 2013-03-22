141489c4474bb6e5002b41a91ebd0e065c4ecd46
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-22 17:08:54 +0000

    list of SQL queries we might need

--- sql/wp_bhaa_houses.sql
+++ sql/wp_bhaa_houses.sql
@@ -9,6 +9,17 @@ update wp_terms set name='inactive',slug='inactive' where name='inactiveteam';
 
 
 -- list the runners in teams
-
+select * from wp_users
+inner join wp_usermeta company on (company.user_id=wp_users.id and company.meta_key='bhaa_runner_company')
+WHERE NOT EXISTS (
+	SELECT * FROM p2p_wp_usermeta standard WHERE standard.user_id=wp_users.id and standard.meta_key='bhaa_runner_standard'
+)
 
 -- list the runners not linked to teams
+-- select non-active company teams with
+
+-- list companies and number of active runner
+
+-- select sector teams with more than 6 runners
+
+-- select active teams without contacts
