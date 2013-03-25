24f2debc764b64fb000a6be52a1fb88f1cf61ff5
emeraldjava <paul.t.oconnell@gmail.com>
2013-03-25 16:12:39 +0000

    SQl to check a users company and p2p listing

--- sql/wp_bhaa_houses.sql
+++ sql/wp_bhaa_houses.sql
@@ -15,6 +15,12 @@ WHERE NOT EXISTS (
 	SELECT * FROM p2p_wp_usermeta standard WHERE standard.user_id=wp_users.id and standard.meta_key='bhaa_runner_standard'
 )
 
+-- runner id, company and if they are linked
+select wp_users.id,company.meta_value,r2c.p2p_from from wp_users
+left join wp_usermeta company ON (company.user_id=wp_users.id AND company.meta_key = 'bhaa_runner_company')
+left join wp_p2p r2c ON (r2c.p2p_to=wp_users.id AND r2c.p2p_type = 'house_to_runner')
+where wp_users.id=11046
+
 -- list the runners not linked to teams
 -- select non-active company teams with
 
