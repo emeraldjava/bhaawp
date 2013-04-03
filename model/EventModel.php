d234f9c0f9b0031d69e97ff3e3659848edf00702
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-03 19:11:19 +0100

    update sql for list runners

--- model/EventModel.php
+++ model/EventModel.php
@@ -40,12 +40,26 @@ class EventModel extends BaseModel
 	{
 		// order by id,
 		return $this->wpdb->get_results(
-			$this->wpdb->prepare('SELECT race,runner,standard,category,racenumber,
-				wp_users.display_name from wp_bhaa_raceresult
-				JOIN wp_p2p e2r ON (wp_bhaa_raceresult.race=e2r.p2p_to AND e2r.p2p_type="event_to_race")
-				JOIN wp_users on (wp_users.id=wp_bhaa_raceresult.runner) 
-				where wp_bhaa_raceresult.class="RACE_REG" 
-				AND e2r.p2p_from=%d order by wp_bhaa_raceresult.id desc',$this->eventid));
+			$this->wpdb->prepare("SELECT race,runner,standard,racenumber,
+firstname.meta_value as firstname,lastname.meta_value as lastname,
+gender.meta_value as gender,dateofbirth.meta_value as dateofbirth,status.meta_value as status,
+house.id as company, house.post_title as companyname, 
+CASE WHEN sector.id IS NOT NULL THEN sector.id ELSE house.id END as teamid,
+CASE WHEN sector.post_title IS NOT NULL THEN sector.post_title ELSE house.post_title END as teamname
+from wp_bhaa_raceresult
+JOIN wp_p2p e2r ON (wp_bhaa_raceresult.race=e2r.p2p_to AND e2r.p2p_type='event_to_race')
+JOIN wp_users on (wp_users.id=wp_bhaa_raceresult.runner) 
+left join wp_p2p r2c ON (r2c.p2p_to=wp_users.id AND r2c.p2p_type = 'house_to_runner')
+left join wp_posts house on (house.id=r2c.p2p_from and house.post_type='house')
+left join wp_p2p r2s ON (r2s.p2p_to=wp_users.id AND r2s.p2p_type = 'sectorteam_to_runner')
+left join wp_posts sector on (sector.id=r2s.p2p_from and house.post_type='house')
+left join wp_usermeta firstname ON (firstname.user_id=wp_users.id AND firstname.meta_key = 'first_name')
+left join wp_usermeta lastname ON (lastname.user_id=wp_users.id AND lastname.meta_key = 'last_name')
+left join wp_usermeta gender ON (gender.user_id=wp_users.id AND gender.meta_key = 'bhaa_runner_gender')
+left join wp_usermeta dateofbirth ON (dateofbirth.user_id=wp_users.id AND dateofbirth.meta_key = 'bhaa_runner_dateofbirth')
+left join wp_usermeta status ON (status.user_id=wp_users.id AND status.meta_key = 'bhaa_runner_status')
+where wp_bhaa_raceresult.class='RACE_REG' 
+AND e2r.p2p_from=%d order by wp_bhaa_raceresult.id desc",$this->eventid));
 	}
 	
 	function getRaces()
