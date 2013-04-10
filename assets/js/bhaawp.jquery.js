ab5ffd6dacdc84bf4018a8be93ff6ece69edfcf2
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-10 17:42:57 +0100

    jquery autocomplete combobox

--- assets/js/bhaawp.jquery.js
+++ assets/js/bhaawp.jquery.js
@@ -12,22 +12,136 @@ jQuery(document).ready( function() {
 			});
 	});
 
-	// http://jqueryui.com/autocomplete/#combobox
-//	jQuery("#bhaa_runner_house").autocomplete({
-//		source: availableTags,
-//	 	minLength: 2,
-//		focus: function(event, ui) {
-//			jQuery("#bhaa_runner_house").val(ui.item.label);
-//        	return false;
-//    	},
-//		select: function(event, ui) {
-//			alert(ui.item.value);
-//			console.debug(ui);
-//			//jQuery("#bhaa_runner_company_name").val(ui.item.label);
-//			//jQuery("#bhaa_runner_company").val(ui.item.value);
-//			return false;
-//		}
-//	});
+	
+	
+//	jQuery.widget( "ui.combobox", {
+//	      _create: function() {
+//	        this.wrapper = jQuery("<span>").addClass( "ui-combobox" ).insertAfter( this.element );
+//	        this._createAutocomplete();
+//	        this._createShowAllButton();
+//	      },
+//	 
+//	      _createAutocomplete: function() {
+//	        var selected = this.element.children( ":selected" ),
+//	          value = selected.val() ? selected.text() : "";
+//	 
+//	        this.input = jQuery( "<input>" )
+//	          .appendTo( this.wrapper )
+//	          .val( value )
+//	          .attr( "title", "" )
+//	          .addClass( "ui-state-default ui-combobox-input ui-widget ui-widget-content ui-corner-left" )
+//	          .autocomplete({
+//	            delay: 0,
+//	            minLength: 0,
+//	            source: jQuery.proxy( this, "_source" )
+//	          });
+////	          .tooltip({
+////	            tooltipClass: "ui-state-highlight"
+////	          });
+//	 
+//	        this._on( this.input, {
+//	          autocompleteselect: function( event, ui ) {
+//	            ui.item.option.selected = true;
+//	            alert(ui.item.label);
+// 				console.debug(ui.item.label);
+// 	        	$("#bhaa_runner_company_name").val(ui.item.label);
+// 				//$("#bhaa_runner_company").val(ui.item.value);
+//	            this._trigger( "select", event, {
+//	            	item: ui.item.option;
+//	            });
+//	          },
+//	 
+//	          autocompletechange: "_removeIfInvalid"
+//	        });
+//	      },
+//	 
+//	      _createShowAllButton: function() {
+//	        var wasOpen = false;
+//	 
+//	        jQuery("<a>")
+//	          .attr( "tabIndex", -1 )
+//	          .attr( "title", "Show All Items" )
+//	          .tooltip()
+//	          .appendTo( this.wrapper )
+//	          .button({
+//	            icons: {
+//	              primary: "ui-icon-triangle-1-s"
+//	            },
+//	            text: false
+//	          })
+//	          .removeClass( "ui-corner-all" )
+//	          .addClass( "ui-corner-right ui-combobox-toggle" )
+//	          .mousedown(function() {
+//	            wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+//	          })
+//	          .click(function() {
+//	            input.focus();
+//	 
+//	            // Close if already visible
+//	            if ( wasOpen ) {
+//	              return;
+//	            }
+//	 
+//	            // Pass empty string as value to search for, displaying all results
+//	            input.autocomplete( "search", "" );
+//	          });
+//	      },
+//	 
+//	      _source: function( request, response ) {
+//	        var matcher = new RegExp( jQuery.ui.autocomplete.escapeRegex(request.term), "i" );
+//	        response( this.element.children( "option" ).map(function() {
+//	          var text = jQuery( this ).text();
+//	          if ( this.value && ( !request.term || matcher.test(text) ) )
+//	            return {
+//	              label: text,
+//	              value: text,
+//	              option: this
+//	            };
+//	        }) );
+//	      },
+//	 
+//	      _removeIfInvalid: function( event, ui ) {
+//	 
+//	        // Selected an item, nothing to do
+//	        if ( ui.item ) {
+//	          return;
+//	        }
+//	 
+//	        // Search for a match (case-insensitive)
+//	        var value = this.input.val(),
+//	          valueLowerCase = value.toLowerCase(),
+//	          valid = false;
+//	        this.element.children( "option" ).each(function() {
+//	          if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+//	            this.selected = valid = true;
+//	            return false;
+//	          }
+//	        });
+//	 
+//	        // Found a match, nothing to do
+//	        if ( valid ) {
+//	          return;
+//	        }
+//	 
+//	        // Remove invalid value
+//	        this.input
+//	          .val( "" )
+//	          .attr( "title", value + " didn't match any item" )
+//	          .tooltip( "open" );
+//	        this.element.val( "" );
+//	        this._delay(function() {
+//	          this.input.tooltip( "close" ).attr( "title", "" );
+//	        }, 2500 );
+//	        this.input.data( "ui-autocomplete" ).term = "";
+//	      },
+//	 
+//	      _destroy: function() {
+//	        this.wrapper.remove();
+//	        this.element.show();
+//	      }
+//	    });
+//
+//	jQuery("#bhaa_runner_company").combobox();
 	
 	// firstname 8
 	// secondname 10
