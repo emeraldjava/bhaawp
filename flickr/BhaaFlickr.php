08c45dab3259d098f7dd68822bb8eba0388ab3c2
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-04-12 00:38:52 +0100

    update flickr php

--- flickr/BhaaFlickr.php
+++ flickr/BhaaFlickr.php
@@ -15,11 +15,37 @@ class BhaaFlickr
 			'foo' => 'something',
 			'bar' => 'something else',
 			), $atts ) );
-		return print_r($this->bhaa_flickr_list_albums());//'bhaa_flickr_shortcode';
+		print_r($this->bhaa_flickr_list_albums(),true);//'bhaa_flickr_shortcode';
 	}
 	
 	function bhaa_flickr_list_albums() {
-		return $this->phpFlickr->photosets_getList(get_option('bhaa_flickr_user_id'));
+		
+		//$list = $this->phpFlickr->photosets_getList(get_option('bhaa_flickr_user_id'));
+		
+		//$person = $this->phpFlickr->people_findByUsername('50904170@N08');//eoinfegan');//get_option('bhaa_flickr_username'));
+		//var_dump($person);
+		
+		// Get the friendly URL of the user's photos
+		$photos_url = $this->phpFlickr->urls_getUserPhotos('50904170@N08');//$person['id']);
+		
+		// Get the user's first 36 public photos
+		$photos = $this->phpFlickr->people_getPublicPhotos($person['id'], NULL, NULL, 36);
+		
+		// Loop through the photos and output the html
+		foreach ((array)$photos['photos']['photo'] as $photo) {
+			echo "<a href=$photos_url$photo[id]>";
+			echo "<img border='0' alt='$photo[title]' ".
+			"src=" . $this->phpFlickr->buildPhotoURL($photo, "Square") . ">";
+			echo "</a>";
+					$i++;
+					// If it reaches the sixth photo, insert a line break
+					if ($i % 6 == 0) {
+					echo "<br>\n";
+		}
+		}
+		
+		//var_dump($list);
+		//return $list;
 	}
 }
 ?>
