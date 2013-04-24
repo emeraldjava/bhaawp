919a8926115857e2a48329724c77fc26118a9742
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-24 18:54:04 +0100

    datepicker

--- assets/js/bhaawp.jquery.js
+++ assets/js/bhaawp.jquery.js
@@ -2,20 +2,20 @@
 jQuery(document).ready( function() {
 	
 	jQuery(function() {
-		jQuery("#bhaa_runner_dateofbirth").datepicker({
+		jQuery("#dateofbirth").datepicker({
 			defaultDate: '-30y', 
 			dateFormat: 'dd/mm/yy',
 			yearRange: '1900:1994',
-			changeYear: 'true',
-			changeMonth: 'true',
-			maxDate: '-18y' 
+			changeYear: true,
+			changeMonth: true
+			//maxDate: '-18y' 
 			});
 		
-		//http://stackoverflow.com/questions/2771137/jquery-date-picker-where-text-input-is-read-only
-		jQuery("#bhaa_runner_dateofbirth").keypress(function (e)
-		{
-			e.preventDefault();
-		});		
+//		//http://stackoverflow.com/questions/2771137/jquery-date-picker-where-text-input-is-read-only
+//		jQuery("#dateofbirth").keypress(function (e)
+//		{
+//			e.preventDefault();
+//		});		
 	});
 	
 	
