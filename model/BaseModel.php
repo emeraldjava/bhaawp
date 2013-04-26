6fe4a8394e58fc8ab8751c9742b0ec3811e1d5ad
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-26 17:27:34 +0100

    add company name

--- model/BaseModel.php
+++ model/BaseModel.php
@@ -21,6 +21,7 @@ status.meta_value as status,
 gender.meta_value as gender,
 dob.meta_value as dob,
 company.meta_value as company,
+house.post_title as companyname,
 standard.meta_value as standard
 from wp_users
 left join wp_usermeta first_name on (first_name.user_id=wp_users.id and first_name.meta_key='first_name')
@@ -29,6 +30,7 @@ left join wp_usermeta status on (status.user_id=wp_users.id and status.meta_key=
 left join wp_usermeta gender on (gender.user_id=wp_users.id and gender.meta_key='bhaa_runner_gender')
 left join wp_usermeta dob on (dob.user_id=wp_users.id and dob.meta_key='bhaa_runner_dateofbirth')
 left join wp_usermeta company on (company.user_id=wp_users.id and company.meta_key='bhaa_runner_company')
+left join wp_posts house on (house.id=company.meta_value and house.post_type='house')
 left join wp_usermeta standard on (standard.user_id=wp_users.id and standard.meta_key='bhaa_runner_standard')
 where status.meta_value in ('M','I') order by id;")
 		);
