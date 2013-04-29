03f05a8f65841b52dc95efa7582135a81da401c1
emeraldjava <paul.t.oconnell@gmail.com>
2013-04-29 18:08:01 +0100

    position 21

--- sql/kclub2013.sql
+++ sql/kclub2013.sql
@@ -5,3 +5,6 @@ select position, runner from wp_bhaa_raceresult where race=2597;
 -- clean up NULL positions
 select * from wp_bhaa_raceresult where position IS NULL
 delete from wp_bhaa_raceresult where position IS NULL
+
+update wp_bhaa_raceresult set runner=5253 where position=8 and race=2597;
+-- 21 position?
