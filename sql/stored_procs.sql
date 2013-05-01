d610173c9beee24fdcaf9b5d3dfaabaa5661bc4a
emeraldjava <paul.t.oconnell@gmail.com>
2013-05-01 15:15:38 +0100

    womens team league summary

--- sql/stored_procs.sql
+++ sql/stored_procs.sql
@@ -437,100 +437,47 @@ RETURN _pointsTotal;
 
 END$$
 
+-- updateTeamLeagueSummary
 DROP PROCEDURE IF EXISTS `updateTeamLeagueSummary`$$
-CREATE DEFINER=`bhaaie`@`localhost` PROCEDURE `updateTeamLeagueSummary`(_leagueId  INT)
+CREATE PROCEDURE `updateTeamLeagueSummary`(_leagueId  INT)
 BEGIN
-DELETE FROM leaguesummary WHERE leagueType='T' and leagueId = _leagueId;
 
-INSERT INTO leaguesummary(
-								leagueid,
-								leaguetype,
-								leagueparticipantid,
-								leaguestandard,
-								leaguedivision,
-								leagueposition,
-                previousleagueposition,
-								leaguescorecount,
-								leaguepoints)
+DELETE FROM wp_bhaa_leaguesummary WHERE leagueType='T' and league = _leagueId;
 
+INSERT INTO wp_bhaa_leaguesummary(
+	league,
+	leaguetype,
+	leagueparticipant,
+	leaguestandard,
+	leaguedivision,
+	leagueposition,
+	leaguescorecount,
+	leaguepoints)
 SELECT
-t1.leagueid,
+t1.league,
 t1.leaguetype,
-t1.leagueparticipantid,
+t1.leagueparticipant,
 t1.leaguestandard as leaguestandard,
 'W' AS leaguedivision,
 @rownum:=@rownum+1 AS leagueposition,
-t1.previousleagueposition,
 t1.leaguescorecount,
-t1.leaguepoints - (SELECT count(1) FROM teamraceresult where team = t1.leagueparticipantid and class='OW' and league = _leagueId) as leaguepoints
+t1.leaguepoints - (SELECT count(1) FROM wp_bhaa_teamresult where team = t1.leagueparticipant and class='OW') as leaguepoints
 FROM
 (
 SELECT
-_leagueId AS leagueid,
+_leagueId AS league,
 'T' AS leaguetype,
-l.team AS leagueparticipantid,
-ROUND(AVG(standardtotal)) AS leaguestandard,
+l.team AS leagueparticipant,
+0 AS leaguestandard,
 0 AS leaguedivision,
-0 AS previousleagueposition,
 SUM(l.leaguescorecount) AS leaguescorecount,
 SUM(l.leaguepoints) AS leaguepoints
 FROM
 (
-SELECT 1 AS leaguescorecount, team, race, standardtotal, MAX(leaguepoints) AS
+SELECT 1 AS leaguescorecount, team, race, MAX(leaguepoints) AS
 leaguepoints
-FROM teamraceresult trr
-WHERE league = _leagueId and class  in ('W','OW')
-GROUP BY team,race
-) l
-GROUP BY l.team
-ORDER BY leaguepoints DESC
-)t1, (SELECT @rownum:=0) t2;
-
-
-
-
-
-
-	INSERT INTO leaguesummary(
-								leagueid,
-								leaguetype,
-								leagueparticipantid,
-								leaguestandard,
-								leaguedivision,
-								leagueposition,
-
-previousleagueposition,
-								leaguescorecount,
-								leaguepoints)
-
-SELECT
-t1.leagueid,
-t1.leaguetype,
-t1.leagueparticipantid,
-COALESCE(t1.leaguestandard,0) as leaguestandard,
-COALESCE((SELECT code FROM division WHERE type='T' AND gender='M' AND t1.leaguestandard
-BETWEEN min AND max),'D') AS leaguedivision,
-@rownum:=@rownum+1 AS leagueposition,
-t1.previousleagueposition,
-t1.leaguescorecount,
-t1.leaguepoints - (SELECT count(1) FROM teamraceresult where team = t1.leagueparticipantid and class='O' and league = _leagueId) as leaguepoints
-FROM
-(
-SELECT
-_leagueId AS leagueid,
-'T' AS leaguetype,
-l.team AS leagueparticipantid,
-ROUND(AVG(standardtotal)) AS leaguestandard,
-0 AS leaguedivision,
-0 AS previousleagueposition,
-SUM(l.leaguescorecount) AS leaguescorecount,
-SUM(l.leaguepoints) AS leaguepoints
-FROM
-(
-SELECT 1 AS leaguescorecount, team, race, standardtotal, MAX(leaguepoints) AS
-leaguepoints
-FROM teamraceresult trr
-WHERE league = _leagueId and class <> 'W' and class <> 'OW'
+FROM wp_bhaa_teamresult trr
+WHERE class  in ('W','OW')
 GROUP BY team,race
 ) l
 GROUP BY l.team
