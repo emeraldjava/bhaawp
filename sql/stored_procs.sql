eb6b36a06562d110407950afc1d3a79f0e9e8968
emeraldjava <paul.t.oconnell@gmail.com>
2013-07-11 09:43:39 +0100

    include RACE_ORG points in the league summary

--- sql/stored_procs.sql
+++ sql/stored_procs.sql
@@ -181,7 +181,7 @@ CASE
     THEN t.standard
 END
 WHERE rr_outer.race = t.race AND rr_outer.runner=t.runner;
--- update meta field
+-- update 'bhaa_runner_standard' meta field
 UPDATE wp_bhaa_raceresult, wp_usermeta
 SET wp_usermeta.meta_value = wp_bhaa_raceresult.poststandard
 WHERE wp_bhaa_raceresult.runner = wp_usermeta.user_id
@@ -378,56 +378,55 @@ DROP PROCEDURE IF EXISTS `updateLeagueData`$$
 CREATE PROCEDURE `updateLeagueData`(_leagueId INT(11))
 BEGIN
 
-	-- should/can we use a temp table here?
-	DELETE FROM wp_bhaa_leaguesummary WHERE league=_leagueId;
+DELETE FROM wp_bhaa_leaguesummary WHERE league=_leagueId;
 
 INSERT INTO wp_bhaa_leaguesummary(league,leaguetype,leagueparticipant,leaguestandard,leaguescorecount,leaguepoints,
 leaguedivision,leagueposition,leaguesummary)
-  SELECT
-  le.id,
-  'I',
-  rr.runner,
-  ROUND(AVG(rr.standard),0),
-  COUNT(rr.race),
-  ROUND(getLeaguePointsTotal(le.id,rr.runner),1) as leaguepoints,
-  'A',
-  1,
-  GROUP_CONCAT( cast( concat(e.ID,':',rr.leaguepoints) AS char ) SEPARATOR ',') AS leaguesummary
-  FROM wp_bhaa_raceresult rr
-  inner join wp_posts r ON rr.race = r.id
-  inner join wp_p2p e2r on (e2r.p2p_type='event_to_race' and e2r.p2p_to=r.ID)
-  inner join wp_posts e ON e2r.p2p_from = e.id
-  inner join wp_p2p l2e on (l2e.p2p_type='league_to_event' and l2e.p2p_to=e.ID)
-  inner JOIN wp_posts le ON l2e.p2p_from = le.id
-  inner JOIN wp_users ru ON rr.runner = ru.id
-  JOIN wp_usermeta status ON (status.user_id=rr.runner AND status.meta_key = 'bhaa_runner_status')
-  JOIN wp_usermeta standard ON (standard.user_id=rr.runner AND standard.meta_key = 'bhaa_runner_standard')
-  WHERE le.id=_leagueId AND class='RAN' AND standard.meta_value IS NOT NULL AND status.meta_value='M'
-  GROUP BY le.id,rr.runner
-  HAVING COALESCE(leaguepoints, 0) > 0;
+SELECT
+le.id,
+'I',
+rr.runner,
+ROUND(AVG(rr.standard),0),
+COUNT(rr.race),
+ROUND(getLeaguePointsTotal(le.id,rr.runner),1) as leaguepoints,
+'A',
+1,
+GROUP_CONCAT( cast( concat(e.ID,':',rr.leaguepoints) AS char ) SEPARATOR ',') AS leaguesummary
+FROM wp_bhaa_raceresult rr
+inner join wp_posts r ON rr.race = r.id
+inner join wp_p2p e2r on (e2r.p2p_type='event_to_race' and e2r.p2p_to=r.ID)
+inner join wp_posts e ON e2r.p2p_from = e.id
+inner join wp_p2p l2e on (l2e.p2p_type='league_to_event' and l2e.p2p_to=e.ID)
+inner JOIN wp_posts le ON l2e.p2p_from = le.id
+inner JOIN wp_users ru ON rr.runner = ru.id
+JOIN wp_usermeta status ON (status.user_id=rr.runner AND status.meta_key = 'bhaa_runner_status')
+JOIN wp_usermeta standard ON (standard.user_id=rr.runner AND standard.meta_key = 'bhaa_runner_standard')
+WHERE le.id=_leagueId AND class in ('RAN','RACE_ORG') AND standard.meta_value IS NOT NULL AND status.meta_value='M'
+GROUP BY le.id,rr.runner
+HAVING COALESCE(leaguepoints, 0) > 0;
   
-	update wp_bhaa_leaguesummary 
-	JOIN wp_usermeta gender ON (gender.user_id=wp_bhaa_leaguesummary.leagueparticipant AND gender.meta_key = 'bhaa_runner_gender')
-	JOIN wp_bhaa_division d ON ((wp_bhaa_leaguesummary.leaguestandard BETWEEN d.min AND d.max) AND d.type='I' and d.gender=gender.meta_value)
-	set wp_bhaa_leaguesummary.leaguedivision=d.code
-	where league=_leagueId;
+update wp_bhaa_leaguesummary 
+JOIN wp_usermeta gender ON (gender.user_id=wp_bhaa_leaguesummary.leagueparticipant AND gender.meta_key = 'bhaa_runner_gender')
+JOIN wp_bhaa_division d ON ((wp_bhaa_leaguesummary.leaguestandard BETWEEN d.min AND d.max) AND d.type='I' and d.gender=gender.meta_value)
+set wp_bhaa_leaguesummary.leaguedivision=d.code
+where league=_leagueId;
 
-	SET @a=0;
-	UPDATE wp_bhaa_leaguesummary SET leagueposition=(@a:= (@a+1)) where leaguedivision="A" and league=_leagueId ORDER BY leaguepoints DESC;
-	SET @b=0;
-	UPDATE wp_bhaa_leaguesummary SET leagueposition=(@b:= (@b+1)) where leaguedivision="B" and league=_leagueId ORDER BY leaguepoints DESC;
-	SET @c=0;
-	UPDATE wp_bhaa_leaguesummary SET leagueposition=(@c:= (@c+1)) where leaguedivision="C" and league=_leagueId ORDER BY leaguepoints DESC;
-	SET @d=0;
-	UPDATE wp_bhaa_leaguesummary SET leagueposition=(@d:= (@d+1)) where leaguedivision="D" and league=_leagueId ORDER BY leaguepoints DESC;
-	SET @e=0;
-	UPDATE wp_bhaa_leaguesummary SET leagueposition=(@e:= (@e+1)) where leaguedivision="E" and league=_leagueId ORDER BY leaguepoints DESC;
-	SET @f=0;
-	UPDATE wp_bhaa_leaguesummary SET leagueposition=(@f:= (@f+1)) where leaguedivision="F" and league=_leagueId ORDER BY leaguepoints DESC;
-	SET @g=0;
-	UPDATE wp_bhaa_leaguesummary SET leagueposition=(@g:= (@g+1)) where leaguedivision="L1" and league=_leagueId ORDER BY leaguepoints DESC;
-	SET @h=0;
-	UPDATE wp_bhaa_leaguesummary SET leagueposition=(@h:= (@h+1)) where leaguedivision="L2" and league=_leagueId ORDER BY leaguepoints DESC;
+SET @a=0;
+UPDATE wp_bhaa_leaguesummary SET leagueposition=(@a:= (@a+1)) where leaguedivision="A" and league=_leagueId ORDER BY leaguepoints DESC;
+SET @b=0;
+UPDATE wp_bhaa_leaguesummary SET leagueposition=(@b:= (@b+1)) where leaguedivision="B" and league=_leagueId ORDER BY leaguepoints DESC;
+SET @c=0;
+UPDATE wp_bhaa_leaguesummary SET leagueposition=(@c:= (@c+1)) where leaguedivision="C" and league=_leagueId ORDER BY leaguepoints DESC;
+SET @d=0;
+UPDATE wp_bhaa_leaguesummary SET leagueposition=(@d:= (@d+1)) where leaguedivision="D" and league=_leagueId ORDER BY leaguepoints DESC;
+SET @e=0;
+UPDATE wp_bhaa_leaguesummary SET leagueposition=(@e:= (@e+1)) where leaguedivision="E" and league=_leagueId ORDER BY leaguepoints DESC;
+SET @f=0;
+UPDATE wp_bhaa_leaguesummary SET leagueposition=(@f:= (@f+1)) where leaguedivision="F" and league=_leagueId ORDER BY leaguepoints DESC;
+SET @g=0;
+UPDATE wp_bhaa_leaguesummary SET leagueposition=(@g:= (@g+1)) where leaguedivision="L1" and league=_leagueId ORDER BY leaguepoints DESC;
+SET @h=0;
+UPDATE wp_bhaa_leaguesummary SET leagueposition=(@h:= (@h+1)) where leaguedivision="L2" and league=_leagueId ORDER BY leaguepoints DESC;
 
 END$$
 
