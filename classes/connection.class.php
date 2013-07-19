6e614f8955f4418e83d17301cecca9a268e5226f
emeraldjava <paul.t.oconnell@gmail.com>
2013-07-19 12:07:11 +0100

    Fix the linking of runners to houses. Need to define the argument types when calling p2p_connection_exists.

--- classes/connection.class.php
+++ classes/connection.class.php
@@ -102,24 +102,27 @@ class Connection {
 		error_log('bhaa_p2p_delete_connections() '.$connection->p2p_type.' '.$connection->p2p_from.' -> '.$connection->p2p_to.'. '.$res);
 	}
 	
-	function updateRunnersHouse($p2p_type,$from,$to)
-	{
-		if(p2p_connection_exists($p2p_type,array($from,$to))) {
+	/**
+	 * Links a runner to a specific house
+	 * @param unknown $p2p_type
+	 * @param unknown $from
+	 * @param unknown $to
+	 * @return Ambigous <boolean, number, mixed>
+	 */
+	function updateRunnersHouse($p2p_type,$from,$to) {
+		//error_log('p2p_create_connection '.$p2p_type.' '.$from.' '.$to);
+		if(p2p_connection_exists($p2p_type,array('from' => $from, 'to' => $to))) {
 			$p2p_id = p2p_get_connections($p2p_type,array($from,$to));
 			$re = p2p_update_meta($p2p_id, $from, $to);
+			error_log('updateRunnersHouse update '.$p2p_type.' '.$from.' '.$to.' '.$re);
 		} else {
-			//$re = p2p_type($p2p_type)->connect( $from, $to );
-			error_log('p2p_create_connection '.$p2p_type.' '.$from.' '.$to);
 			$re = p2p_create_connection($p2p_type, array(
 					'from' => $from,
 					'to' => $to));
-			
-			//array($from,$to));
+			error_log('updateRunnersHouse create '.$p2p_type.' '.$from.' '.$to.' '.$re);
 		}
 		if ( is_wp_error($re) )
 			error_log($re->get_error_message());
-		//	$this->displayAndLog($re->get_error_message());
-		error_log('updateRunnersHouse '.$p2p_type.' '.$from.' '.$to.' '.$re);
 		return $re;
 	}
 }
