f9a57da572fb69f2d393b455677aaa74e8a06d99
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-09-05 19:00:27 +0100

    disable the custom content action

--- bhaawp.php
+++ bhaawp.php
@@ -1,16 +1,24 @@
 <?php
 /*
+
 Plugin Name: BHAA Plugin
+
 Plugin URI: https://github.com/emeraldjava/bhaawp
+
 Description: Plugin to handle bhaa results
+
 Version: 2013.09.04
+
 Author: paul.t.oconnell@gmail.com
+
 Author URI: https://github.com/emeraldjava/bhaawp
+
 */
 
 class BHAA
 {
 	var $version = '2013.09.04';
+
 	
 	var $connection;
 	
@@ -26,22 +34,36 @@ class BHAA
 	var $raceday;
 	
 	function __construct()
+
 	{
+
 		//global $wpdb;
+
 		//$wpdb->show_errors();
+
 		//$this->loadOptions();
+
 		$this->defineConstants();
+
 		//$this->loadTextdomain();
+
 		$this->loadLibraries();
+
 	
+
 		//add_action( 'widgets_init', array(&$this, 'registerWidget') );
+
 		// Start this plugin once all other plugins are fully loaded
+
 		//add_action( 'plugins_loaded', array(&$this, 'initialize') );
+
 		//add_action( 'p2p_init', array(&$this->connection,'bhaa_connection_types'));
+
 		
 		if ( is_admin() )
 		{
 			register_activation_hook(__FILE__, array($this, 'bhaa_activate') );
+
 			register_uninstall_hook(__FILE__, array($this, 'bhaa_uninstall'));
 			new BhaaAdmin();
 		}
@@ -49,10 +71,11 @@ class BHAA
 		// init, wp_head, wp_enqueue_scripts
 		add_action('init', array($this,'enqueue_scripts_and_style'));
 		
-		add_filter('the_content', array($this,'bhaa_content'));
-		add_action('init',array($this,'bhaa_form_actions'));
+		//add_filter('the_content', array($this,'bhaa_content'));
+		//add_action('init',array($this,'bhaa_form_actions'));
 				
 		// hook add_query_vars function into query_vars
+
 		add_filter('query_vars', array($this,'add_query_vars'));
 	}
 	
@@ -69,6 +92,7 @@ class BHAA
 	 */
 	function bhaa_content($page_content) {
 		global $post, $wpdb, $wp_query;
+
 		if( empty($post) ) 
 			return $page_content; 
 		
@@ -92,47 +116,81 @@ class BHAA
 		
 	function bhaa_locate_template( $template_name, $load=false, $args = array() ) {
 		//First we check if there are overriding tempates in the child or parent theme
+
 		$located = locate_template(array('plugins/bhaawp-master/'.$template_name));
 		if( !$located ) {
 			if ( file_exists(BHAA_PLUGIN_DIR.'/templates/'.$template_name) ) {
+
 				$located = BHAA_PLUGIN_DIR.'/templates/'.$template_name;
 			}
+
 		}
 		$located = apply_filters('bhaa_locate_template', $located, $template_name, $load, $args);
+
 		if( $located && $load ) {
+
 			if( is_array($args) ) 
 				extract($args);
+
 			include($located);
+
 		}
+
 		return $located;
+
 	}
 	
 	/**
 	 * http://codex.wordpress.org/Function_Reference/load_template
+
 	 * http://keithdevon.com/passing-variables-to-get_template_part-in-wordpress/
+
 	 * https://github.com/stephenharris/Event-Organiser/blob/1.7.3/includes/event-organiser-templates.php#L193
+
 	 */
+
 /* 	function bhaa_locate_template($template_names, $load = false, $require_once = true ) {
+
 		$located = '';
+
 	
+
 		$template_dir = get_stylesheet_directory(); //child theme
+
 		$parent_template_dir = get_template_directory(); //parent theme
+
 	
+
 		$stack = array( $template_dir, $parent_template_dir, BHAA_PLUGIN_DIR . 'templates' );
+
 	
+
 		foreach ( (array) $template_names as $template_name ) {
+
 			if ( !$template_name )
+
 				continue;
+
 			foreach ( $stack as $template_stack ){
+
 				if ( file_exists( trailingslashit( $template_stack ) . $template_name ) ) {
+
 					$located = trailingslashit( $template_stack ) . $template_name;
+
 					break;
+
 				}
+
 			}
+
 		}
+
 		if ( $load && '' != $located )
+
 			load_template( $located, $require_once );
+
 		return $located;
+
 	} */
 	
 	
@@ -142,8 +200,11 @@ class BHAA
 	 * @return string
 	 */
 	function add_query_vars($aVars) {
+
 		$aVars[] = "division"; // represents the name of the product category as shown in the URL
+
 		return $aVars;
+
 	}
 
 	/**
@@ -172,6 +233,7 @@ if ( file_exists ( LG_FE_DIR . "/includes/chart_templates/class.{$class}.php" ) 
 		
 		new LeagueCpt();
 		new RaceCpt();
+
 		new HouseCpt();
 		
 		new BHAAEventManager();
@@ -180,6 +242,7 @@ if ( file_exists ( LG_FE_DIR . "/includes/chart_templates/class.{$class}.php" ) 
 		$this->individualResultTable = new RaceResultTable();
 		//$this->teamResultTable = new TeamResultTable();
 		
+
 		$this->runner = new Runner();
 		$this->event = new Event();
 		$this->registration = new Registration();
@@ -201,18 +264,27 @@ if ( file_exists ( LG_FE_DIR . "/includes/chart_templates/class.{$class}.php" ) 
 	}
 	
 	function getRaceResult() {
+
 		return new RaceResult();
+
 	}
+
 	
+
 	function getIndividualResultTable()
+
 	{
+
 		return $this->individualResultTable;
+
 	}
 	
 	public function getTeamResultTable($race)
+
 	{
 		$teamResult = new TeamResult($race);
 		return $teamResult->getTeamTable();
+
 	}
 	
 	/**
@@ -225,20 +297,29 @@ if ( file_exists ( LG_FE_DIR . "/includes/chart_templates/class.{$class}.php" ) 
 	function enqueue_scripts_and_style()
 	{
 		
+
 		// declare the URL to the file that handles the AJAX request (wp-admin/admin-ajax.php)
+
 		//wp_enqueue_script( 'my-ajax-request', plugin_dir_url( __FILE__ ) . 'js/ajax.js', array( 'jquery' ) );
+
 		//wp_localize_script( 'my-ajax-request', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+
 		
 		// http://wordpress.stackexchange.com/questions/56343/template-issues-getting-ajax-search-results/56349#56349
  		wp_register_script(
  			'bhaawp', 
  			plugins_url('assets/js/bhaawp.jquery.js',__FILE__),
  			array('jquery','jquery-ui-core','jquery-ui-widget','jquery-ui-position','jquery-ui-sortable','jquery-ui-datepicker','jquery-ui-autocomplete','jquery-ui-dialog'));
+
  		wp_enqueue_script('bhaawp');
 // 		wp_register_script(
+
 	// 		'bootstrap-js',
+
 	 //		plugins_url('assets/js/bootstrap.min.js',__FILE__),
+
 	 	//	array('jquery'));
+
  		wp_enqueue_script('bootstrap-js');
  		wp_localize_script(
  			'bhaawp',
@@ -257,35 +338,56 @@ if ( file_exists ( LG_FE_DIR . "/includes/chart_templates/class.{$class}.php" ) 
  			//'jquery-bhaa-style',
  			//'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
 		wp_enqueue_style(
+
 			'bhaawp',
+
 			plugins_url() . '/bhaawp-master/assets/css/bhaawp.css',
+
 			false);
 		wp_enqueue_style(
  			'jquery-bhaa-style',
+
  			plugins_url() . '/bhaawp-master/assets/css/jqueryui/jquery-ui-1.10.3.custom.min.css',
+
  			false);
  		/*wp_enqueue_style(
+
 	 		'bootstrap-css',
+
 	 		plugins_url() . '/bhaawp-master/assets/css/bootstrap.min.css',
+
 	 		false); */
 	}
 		
 	function bhaawp_house_search() {
 		error_log('bhaawp_house_search '.$_REQUEST['term']);
+
 		$posts = get_posts( array(
 			's' => trim( esc_attr( strip_tags( $_REQUEST['term'] ) ) ),
 			'post_type' => 'house'
+
 		) );
+
 		$suggestions=array();
+
 	
+
 		global $post;
+
 		foreach ($posts as $post):
+
 		setup_postdata($post);
+
 		$suggestion = array();
+
 		$suggestion['label'] = esc_html($post->post_title);
+
 		$suggestion['link'] = get_permalink();
+
 		$suggestions[]= $suggestion;
+
 		endforeach;
+
 	
 		wp_reset_postdata();
 		
@@ -294,13 +396,19 @@ if ( file_exists ( LG_FE_DIR . "/includes/chart_templates/class.{$class}.php" ) 
 		echo $response;
 		
 		die();
+
  		//exit;
+
 	}
 				
 	function defineConstants()
+
 	{
+
 		define('BHAAWP_PATH', plugin_dir_path(__FILE__));
+
 		global $wpdb;
+
 		$wpdb->raceresult 	= $wpdb->prefix.'bhaa_raceresult';
 		$wpdb->teamresult 	= $wpdb->prefix.'bhaa_teamresult';
 		$wpdb->importTable = $wpdb->prefix.'bhaa_import';
@@ -308,28 +416,44 @@ if ( file_exists ( LG_FE_DIR . "/includes/chart_templates/class.{$class}.php" ) 
 	}
 	
 	function bhaa_activate()
+
 	{
+
 		global $wpdb;
 		
 		$options = array();
+
 		add_option( 'bhaa', $options, 'BHAA Options', 'yes' );
+
 		//add_option( 'bhaa_widget', array(), 'BHAA Widget Options', 'yes' );
+
 
 		// raceresult SQL
 		$raceResult = new RaceResult();
 		$this->run_install_or_upgrade($raceResult->getName(),$raceResult->getCreateSQL());
 		
 		$teamResultSql = "
+
 			id int(11) NOT NULL AUTO_INCREMENT,
+
 			team int(11) NOT NULL,
+
 			league int(11) NOT NULL,
+
 			race int(11) NOT NULL,
+
 			standardtotal int(11),
+
 			positiontotal int(11),
+
 			class enum('A', 'B', 'C', 'D', 'W', 'O', 'OW'),
+
 			leaguepoints int(11),
+
 			status enum('ACTIVE','PENDING'),
+
 			PRIMARY KEY (id)";
+
 		$this->run_install_or_upgrade($wpdb->teamresult,$teamResultSql);
 		
 		$leagueSummaryModel = new LeagueSummary();
@@ -339,56 +463,93 @@ if ( file_exists ( LG_FE_DIR . "/includes/chart_templates/class.{$class}.php" ) 
 		$this->run_install_or_upgrade($ageCategory->getName(),$ageCategory->getCreateSQL());
 		
 		$importTableSql = "
+
 			id int(11) NOT NULL AUTO_INCREMENT,
 			tag varchar(15) NOT NULL,
+
 			type varchar(15) NOT NULL,
+
 			new int(11) NOT NULL,
+
 			old int(11) NOT NULL,
+
 			PRIMARY KEY (id)";
+
 		$this->run_install_or_upgrade($wpdb->importTable,$importTableSql);
 		// populate the table with the data
 		$this->run_install_or_upgrade($wpdb->standardTable,$this->standardCalculator->standardTableSql);
 		foreach ($this->standardCalculator->standards as $i => $standard) {
 			$wpdb->insert( $wpdb->standardTable, (array)$standard );
 		}
+
 	}
 	
 	function run_install_or_upgrade($table_name, $sql)//, $db_version)
+
 	{
+
 		global $wpdb;
+
 		// Table does not exist, we create it!
+
 		// We use InnoDB and UTF-8 by default
+
 		if ($wpdb->get_var("SHOW TABLES LIKE '".$table_name."'") != $table_name)
+
 		{
+
 			$create = "CREATE TABLE ".$table_name." ( ".$sql." ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
 			error_log($create);
+
 			// We use the dbDelta method given by WP!
+
 			require_once ABSPATH.'wp-admin/includes/upgrade.php';
+
 			dbDelta($create);
+
 		}
+
 	}
 	
 	function bhaa_uninstall()
 	{	
 		global $wpdb;
+
 		
+
 		// tables
+
 		$tables = array(
+
 			//$wpdb->raceresult
+
 		);
+
 		
+
 		// Delete each table one by one
+
 		foreach ($tables as $table)
+
 		{
+
 			$wpdb->query('DROP TABLE IF EXISTS '.$table.';');
+
 		}
+
 			
+
 		delete_option( 'bhaa_widget' );
+
 		delete_option( 'bhaa' );
+
 	}
 }
 
 // Run the Plugin
+
 global $BHAA;
+
 $BHAA = new BHAA();
 ?>
