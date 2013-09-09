5c0dc4c166367e40d6443d0307ce9457d61720dc
emeraldjava <paul.t.oconnell@gmail.com>
2013-09-09 11:09:35 +0100

    update email test script

--- test/email.php
+++ test/email.php
@@ -3,20 +3,6 @@ class Email
 {
 	function send($name)
 	{
- 		
-		
-// 		$line = "a,b,c,
-// 		d,e,f,";
-		
-// 		$parsed = str_getcsv(
-// 				$line, # Input line
-// 				',',   # Delimiter
-// 				'"',   # Enclosure
-// 				'\n'   # Escape char
-// 				);
-// 		var_dump( $parsed );
-
-// 		var_dump( time() );
  		$headers = 'MIME-Version: 1.0' . "\r\n";
  		$headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
  		$headers .= 'From: oconnellp@aegon.ie' . "\r\n";
@@ -24,17 +10,7 @@ class Email
  		$email = "oconnellp@aegon.ie";
  		echo "hi ".$headers;
  		$res = mail($email,"test subject 3 ".$email,"test body 3 ".$email,$headers);
-		echo $res;
-		
-// 		$args = array(
-// 				'post_type'=> 'company',
-// 				'name'    => $_REQUEST['term']);
-// 		error_log('bhaawp_company_search '.$_REQUEST['term']);
-// 		//		query_posts($args);
-		
-// 		$query = new WP_Query($args);//'pagename=contact' );
-		
-// 		error_log('bhaawp_company_search '.$query);
+ 		echo 'result: '.$res;
 	}
 }
 
