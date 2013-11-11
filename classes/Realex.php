b6a8612789dab7a00107e15c9898f04f2b65c828
emeraldjava <paul.t.oconnell@gmail.com>
2013-11-11 16:53:38 +0000

    handle post in a seperate method

--- classes/Realex.php
+++ classes/Realex.php
@@ -4,23 +4,33 @@
  * curl -v -X POST --data "a=b&c=d&MD5HASH=sadsd" http://localhost/realex
  *
  * @author oconnellp
+ * 
+ * http://bandhattonbutton.com/payment-result/
+ * 
  */
 class Realex {
 	
 	function process() {
 				
 		if($_SERVER['REQUEST_METHOD']=='POST') {
+			return $this->handle_post();
+		}
+		else {
 			$out = '<h1>Custom Realex Class - '.$_SERVER['REQUEST_METHOD'].'</h1>';
 			$out .= '<h2>';
 			$out .= print_r($_REQUEST,true);
 			$out .= '</h2>';
-			$out .= '<h2>MD5HASH : '.$_POST['MD5HASH'].'</h2>';
-			return $out;
-		}
-		else {
-			$out = '<h1>Custom Realex Class - '.$_SERVER['REQUEST_METHOD'].'</h1>';
 			return $out;			
 		}
+	}
+	
+	private function handle_post() {
+		$out = '<h1>Custom Realex Class - '.$_SERVER['REQUEST_METHOD'].'</h1>';
+		$out .= '<h2>';
+		$out .= print_r($_REQUEST,true);
+		$out .= '</h2>';
+		$out .= '<h2>MD5HASH : '.$_POST['MD5HASH'].'</h2>';
+		return $out;
 	}
 }
 ?>
