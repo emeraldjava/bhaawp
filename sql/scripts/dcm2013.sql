0ced2b1928a07b371158b8fef3ee6316e2c51ac2
emeraldjava <paul.t.oconnell@gmail.com>
2013-11-19 18:05:32 +0000

    55

--- sql/scripts/dcm2013.sql
+++ sql/scripts/dcm2013.sql
@@ -1,3 +1,4 @@
+-- http://dublinmarathon.ie/results/
 
 INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2856,11046,'02:45:13','RAN');
 INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2856,22835,'03:19:38','RAN');
@@ -9,7 +10,12 @@ INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2856,7377,'04
 INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2856,7373,'04:36:34','RAN');
 INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (10,2856,23990,'03:19:38','RAN');
 
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (13,2856,4738,'02:29:30','RAN');
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (16,2856,5622,'02:32:03','RAN');
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (25,2856,1716,'02:34:52','RAN');
 
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (33,2856,5253,'02:36:47','RAN');
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (52,2856,7327,'02:39:18','RAN');
 call updatePositions(2856);
 
 
