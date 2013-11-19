f052f50a13731da164786495c7919f03dcf0a1e5
emeraldjava <paul.t.oconnell@gmail.com>
2013-11-19 17:36:38 +0000

    dcm 2013 rte

---
+++ sql/scripts/dcm2013.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2856,11046,'02.45.13','RAN');
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,22835,'03.19.38','RAN');
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,22858,'03.19.36','RAN');
+
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,7370,'03.44.20','RAN');
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,22860,'03.52.42','RAN');
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,22854,'03.59.03','RAN');
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,7377,'04.22.48','RAN');
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,7373,'04.36.34','RAN');
+
+INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,,'03.19.38','RAN');xDonagh McGrath
+
+
+
