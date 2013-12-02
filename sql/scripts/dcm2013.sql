9d1c8f81bd37ea7cd67bb4cc31556a92dc39d711
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-12-02 22:12:51 +0000

    update dcm 2013

--- sql/scripts/dcm2013.sql
+++ sql/scripts/dcm2013.sql
@@ -1,3 +1,4 @@
+
 -- http://dublinmarathon.ie/results/
 
 INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2856,11046,'02:45:13','RAN');
@@ -16,6 +17,6 @@ INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (25,2
 
 INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (33,2856,5253,'02:36:47','RAN');
 INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (52,2856,7327,'02:39:18','RAN');
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (1801,2856,6549,'03:28:30','RAN');
+
 call updatePositions(2856);
-
-
