c2660718660f4d427dcc7a85f3a2f3e7fe64dbb7
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-12-15 20:32:15 +0000

    sql stuff

--- sql/scripts/dublinhalf2013custom.sql
+++ sql/scripts/dublinhalf2013custom.sql
@@ -62,11 +62,12 @@ INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,7016,'02
 INSERT INTO wp_bhaa_raceresult(race,runner,racetime,class) VALUES (2855,6933,'02:51:44','RAN');
 
 INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (1300,2855,5101,'01:44:53','RAN');
-INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (1300,2855,8732,'01.48.39','RAN');
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (1300,2855,8732,'01:48:39','RAN');
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (1,2855,7088,'01:08:21','RAN');
+INSERT INTO wp_bhaa_raceresult(position,race,runner,racetime,class) VALUES (7175,2855,7175,'03:28:21','RAN');
+call updatePositions(2855);
 
 delete from wp_bhaa_raceresult where race=2855 and runner=6933;
-
-call updatePositions(2855);
 
 -- teams
 select * from wp_bhaa_raceresult rr
