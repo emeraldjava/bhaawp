fd3b70befe79765f2cb410d8f626c5560fc9e54d
emeraldjava <paul.t.oconnell@gmail.com>
2013-12-19 10:20:29 +0000

    Add event admin class

---
+++ admin/class-event-admin.php
@@ -0,0 +1,16 @@
+<?php
+class EventAdmin {
+	protected static $instance = null;
+	
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+	
+	private function __construct() {
+	}
+}
+?>
