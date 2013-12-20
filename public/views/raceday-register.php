bf00f27a451523583a660b11eb8af22bf1c3d3bc
emeraldjava <paul.t.oconnell@gmail.com>
2013-12-20 14:38:03 +0000

    Add a simple wp-form

--- public/views/raceday-register.php
+++ public/views/raceday-register.php
@@ -3,5 +3,6 @@ include_once 'raceday-header.php';
 echo 'BHAA Raceday Register';
 echo '<div class="navbar-search pull-left" align="left">
 <input size="35" type="text" placeholder="Search BHAA Member by Name OR ID" id="memberfilter"/>
-</div>';
+</div><br/>';
+echo wp_get_form('bhaa-register-form');
 ?>
