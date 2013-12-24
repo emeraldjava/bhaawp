d4f261c2b5d7254bab7bdccec5ad28e423063686
Paul O'Connell <paul.t.oconnell@gmail.com>
2013-12-24 14:01:08 +0000

    common tabs

--- sql/event_manager.sql
+++ sql/event_manager.sql
@@ -1,22 +1,10 @@
 update wp_options
-set option_value='<p eventid="#_EVENTID" postid="#_EVENTPOSTID">
-#_EVENTNAME
-
-{is_future}event is in the future{/is_future}
-
-{is_past}event is in the past
+set option_value='<div eventid="#_EVENTID" postid="#_EVENTPOSTID">
+<p>#_EVENTNAME</p>
 [fusion_tabs layout="horizontal" backgroundcolor="" inactivecolor=""]
-[fusion_tab id="results"]#_BHAARACERESULTS[/tab]
-[fusion_tab id="teams"]#_BHAATEAMRESULTS[/tab]
-[fusion_tab id="standards"]
-<h3>BHAA Standard Table</h3>
-<p>Like a golf handicap the BHAA standard table gives a runner a target time for the race distance</p>
-<p>#_BHAASTANDARDS</p>
-[/fusion_tab]
-[/fusion_tabs]
-{/is_past}			
-</p>
 
+{is_future}
+[fusion_tab title="Details"]
 <div style="float:right; margin:0px 0px 15px 15px;">#_LOCATIONMAP</div>
 <p>
 	<strong>Date/Time</strong><br/>
@@ -24,8 +12,9 @@ set option_value='<p eventid="#_EVENTID" postid="#_EVENTPOSTID">
 </p>
 {has_location}
 <p>
-	<strong>Location</strong><br/>
-	#_LOCATIONLINK
+<strong>Location</strong><br/>
+#_LOCATIONLINK
+#_LOCATIONNOTES
 </p>
 {/has_location}
 <p>
@@ -37,5 +26,22 @@ set option_value='<p eventid="#_EVENTID" postid="#_EVENTPOSTID">
 {has_bookings}
 <h3>Bookings</h3>
 #_BOOKINGFORM
-{/has_bookings}'
-where option_name='bhaaie_wp';
+{/has_bookings}
+[/fusion_tab]
+{/is_future}
+
+{is_past}
+[fusion_tab title="Results"]#_BHAARACERESULTS[/fusion_tab]
+[fusion_tab title="Teams"]#_BHAATEAMRESULTS[/fusion_tab]
+[fusion_tab title="Details"]Details[/fusion_tab]
+{/is_past}			
+
+[fusion_tab title="Standards"]
+<h3>BHAA Standard Table</h3>
+<p>Like a golf handicap the BHAA standard table gives a runner a target time for the race distance</p>
+<p>#_BHAASTANDARDS</p>
+[/fusion_tab]
+[/fusion_tabs]
+</div>
+'
+where option_name='dbem_single_event_format';
