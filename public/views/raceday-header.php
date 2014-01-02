5f2cfebe257ae5e0204bad4401328308db53830d
emeraldjava <paul.t.oconnell@gmail.com>
2014-01-02 14:07:12 +0000

    create a raceday table menu

--- public/views/raceday-header.php
+++ public/views/raceday-header.php
@@ -1,10 +1,15 @@
 <?php 
-echo '<ul>
-<li><a href="/raceday-register/">Register</a></li>
-<li><a href="/raceday-newmember/">New/Day Member</a></li>
-<li><a href="/raceday-list/">List</a></li>
-<li><a href="/raceday-latest/">Latest</a></li>
-<li><a href="/raceday-export/">Export</a></li>
-<li><a href="/raceday-prereg/">Pre-Registered</a></li>
-</ul>';
+echo '<table style="width:100%">
+<tr style="line-height: 14px; padding-bottom: 1em;">
+<td style="width=33%"><h3><a href="/raceday-register/">Register</a></h3></td>
+<td style="width=33%"><h3><a href="/raceday-newmember/">New/Day Member</a></h3></td>		
+<td style="width=33%"><h3><a href="/raceday-prereg/">Pre-Registered</a></h3></td>
+</tr>
+<tr style="line-height: 14px; padding-bottom: 1em;">
+<td style="width=33%"><h4><a href="/raceday-tdst/">List</a></h4></td>
+<td style="width=33%"><h4><a href="/raceday-latest/">Latest</a></h4></td>
+<td style="width=33%"><h4><a href="/raceday-export/">Export</a></h4></td>
+</tr>
+</table>
+<hr/>';
 ?>
