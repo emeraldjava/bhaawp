c40533e800fae67d0a54ec698f7374a38f282e85
emeraldjava <paul.t.oconnell@gmail.com>
2014-01-07 18:34:54 +0000

    use the house_to_runner and sectorteam_to_runner mapping for the export

--- sql/users.sql
+++ sql/users.sql
@@ -68,7 +68,36 @@ left join wp_usermeta company ON (company.user_id=wp_users.id AND company.meta_k
 left join wp_posts companyname on (companyname.id=company.meta_value and companyname.post_type='house')
 where wp_bhaa_raceresult.class='RACE_REG' 
 
+SELECT wp_bhaa_raceresult.id,runner,racenumber,race,
+firstname.meta_value as firstname,lastname.meta_value as lastname,
+gender.meta_value as gender,dateofbirth.meta_value as dateofbirth,
+standard.meta_value as standard,status.meta_value as status,
+house.id as companyid, 
+house.post_title as companyname,
+r2c.p2p_from as teamid,
+r2s.p2p_from as sectorteamid,
+standardscoringset
+from wp_bhaa_raceresult
+JOIN wp_p2p e2r ON (wp_bhaa_raceresult.race=e2r.p2p_to AND e2r.p2p_type='event_to_race')
+JOIN wp_users on (wp_users.id=wp_bhaa_raceresult.runner) 
+left join wp_p2p r2c ON (r2c.p2p_to=wp_users.id AND r2c.p2p_type = 'house_to_runner')
+left join wp_p2p r2s ON (r2s.p2p_to=wp_users.id AND r2s.p2p_type = 'sectorteam_to_runner')
+left join wp_posts house on (house.id=r2c.p2p_from and house.post_type='house')
+left join wp_posts sector on (sector.id=r2s.p2p_from and house.post_type='house')
+left join wp_usermeta firstname ON (firstname.user_id=wp_users.id AND firstname.meta_key = 'first_name')
+left join wp_usermeta lastname ON (lastname.user_id=wp_users.id AND lastname.meta_key = 'last_name')
+left join wp_usermeta gender ON (gender.user_id=wp_users.id AND gender.meta_key = 'bhaa_runner_gender')
+left join wp_usermeta dateofbirth ON (dateofbirth.user_id=wp_users.id AND dateofbirth.meta_key = 'bhaa_runner_dateofbirth')
+left join wp_usermeta status ON (status.user_id=wp_users.id AND status.meta_key = 'bhaa_runner_status')
+left join wp_usermeta standard ON (standard.user_id=wp_users.id AND standard.meta_key = 'bhaa_runner_standard')
+where wp_bhaa_raceresult.class='RACE_REG' 
 
+--CASE WHEN house.post_title IS NULL THEN companyname.post_title ELSE house.post_title END as companyname,
+--CASE WHEN sector.id IS NOT NULL THEN sector.id ELSE house.id END as teamid,
+--CASE WHEN sector.post_title IS NOT NULL THEN sector.post_title ELSE house.post_title END as teamname,
+
+--left join wp_usermeta company ON (company.user_id=wp_users.id AND company.meta_key = 'bhaa_runner_company')
+--left join wp_posts companyname on (companyname.id=company.meta_value and companyname.post_type='house')
 
 
 -- create table
