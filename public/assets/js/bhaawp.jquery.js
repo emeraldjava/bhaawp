0d23b2dc6e873d5e7e98a55896df23572491c157
emeraldjava <paul.t.oconnell@gmail.com>
2014-01-10 18:33:26 +0000

    humm raceday autocomplete is not behaving

--- public/assets/js/bhaawp.jquery.js
+++ public/assets/js/bhaawp.jquery.js
@@ -292,13 +292,13 @@ jQuery(document).ready( function() {
 			}
 			return false;	
 		}
-	})
-	.data("ui-autocomplete")._renderItem = function(ul,item){
-		return jQuery("<li></li>")
-	    	.append("<a>"+item.label+" "+item.id+"</a><small>DOB:"+item.dob+", Status:"+item.status+", Company:"+item.companyname+"</small>")
-	    	.data("ui-autocomplete-item", item)
-			.appendTo(ul);
-	};
+	});
+	//.data("ui-autocomplete")._renderItem = function(ul,item){
+	//	return jQuery("<li></li>")
+	//   	.append("<a>"+item.label+" "+item.id+"</a><small>DOB:"+item.dob+", Status:"+item.status+", Company:"+item.companyname+"</small>")
+	//    	.data("ui-autocomplete-item", item)
+	//		.appendTo(ul);
+	//};
 	/**
 	 * http://jqueryui.com/autocomplete/#custom-data
 	 * http://stackoverflow.com/questions/14461787/jqueryui-1-10-0-autocompleter-renderitem-problems
