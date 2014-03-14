72201447436d458be7169da4b9c99f26093641eb
emeraldjava <paul.t.oconnell@gmail.com>
2014-03-14 15:36:18 +0000

    drop the logging

--- includes/model/class-eventmodel.php
+++ includes/model/class-eventmodel.php
@@ -66,7 +66,7 @@ order by ".$order,$class);// AND e2r.p2p_from=%d,$this->eventid);
 		
 		if($limit!=0)
 			$SQL .= " limit ".$limit;
-		error_log($SQL);
+		//error_log($SQL);
 		return $this->wpdb->get_results($SQL);
 	}
 	
