208179cb71d07a554f74bf632e92927cc626a8ea
emeraldjava <paul.t.oconnell@gmail.com>
2014-03-21 12:34:05 +0000

    add race details

--- sql/scripts/wp_bhaa_race_detail.sql
+++ sql/scripts/wp_bhaa_race_detail.sql
@@ -21,12 +21,16 @@ event.ID as event,
 event.post_title as eventname,
 event.post_date as eventdate,
 race.ID as race,
-racetype.meta_value as racetype
+racetype.meta_value as type,
+racedistance.meta_value as distance,
+raceunit.meta_value as raceunit
 from wp_p2p l2e
 join wp_posts event on (l2e.p2p_to=event.ID)
 join wp_p2p e2r on (l2e.p2p_to=e2r.p2p_from AND e2r.p2p_type='event_to_race')
 join wp_posts race on (e2r.p2p_to=race.ID)
-join wp_postmeta racetype on (race.ID=racetype.post_id AND racetype.meta_key='bhaa_race_type')
+LEFT join wp_postmeta racetype on (race.ID=racetype.post_id AND racetype.meta_key='bhaa_race_type')
+LEFT join wp_postmeta racedistance on (race.ID=racedistance.post_id AND racedistance.meta_key='bhaa_race_distance')
+LEFT join wp_postmeta raceunit on (race.ID=raceunit.post_id AND raceunit.meta_key='bhaa_race_unit')
 where l2e.p2p_type='league_to_event' and l2e.p2p_from=3103
 ORDER BY eventdate;
 
