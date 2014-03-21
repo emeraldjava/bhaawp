a30cd2cd439a659ad35dbfa922bce6ea903929ff
emeraldjava <paul.t.oconnell@gmail.com>
2014-03-21 12:35:58 +0000

    Add league type

--- sql/scripts/wp_bhaa_race_detail.sql
+++ sql/scripts/wp_bhaa_race_detail.sql
@@ -17,11 +17,12 @@ DELETE FROM wp_bhaa_race_detail;
 
 select 
 l2e.p2p_from as league,
+leaguetype.meta_value as leaguetype,
 event.ID as event,
 event.post_title as eventname,
 event.post_date as eventdate,
 race.ID as race,
-racetype.meta_value as type,
+racetype.meta_value as racetype,
 racedistance.meta_value as distance,
 raceunit.meta_value as raceunit
 from wp_p2p l2e
@@ -31,6 +32,7 @@ join wp_posts race on (e2r.p2p_to=race.ID)
 LEFT join wp_postmeta racetype on (race.ID=racetype.post_id AND racetype.meta_key='bhaa_race_type')
 LEFT join wp_postmeta racedistance on (race.ID=racedistance.post_id AND racedistance.meta_key='bhaa_race_distance')
 LEFT join wp_postmeta raceunit on (race.ID=raceunit.post_id AND raceunit.meta_key='bhaa_race_unit')
+LEFT join wp_postmeta leaguetype on (l2e.p2p_from=leaguetype.post_id AND leaguetype.meta_key='bhaa_league_type')
 where l2e.p2p_type='league_to_event' and l2e.p2p_from=3103
 ORDER BY eventdate;
 
