78162f290e43caf19bf39f132ebffa2ff2dce86e
emeraldjava <paul.t.oconnell@gmail.com>
2014-03-21 12:44:41 +0000

    Add second league id

--- sql/scripts/wp_bhaa_race_detail.sql
+++ sql/scripts/wp_bhaa_race_detail.sql
@@ -1,20 +1,23 @@
 
 -- wp_bhaa_race_detail table
-DROP TABLE wp_bhaa_race_detail
+DROP TABLE wp_bhaa_race_detail;
 CREATE TABLE wp_bhaa_race_detail (
 	id int(11) auto_increment primary key,
 	league int(11) NULL,
+	leaguetype varchar(1) NULL,
 	event int(11) NULL,
-	eventname varchar(20) NULL,
-	eventdate varchar(20) NULL,
+	eventname varchar(40) NULL,
+	eventdate date NULL,
 	race int(11) NULL,
 	racetype varchar(1) NULL,
-	racedistance double
+	distance varchar(4),
+	unit varchar(4) NULL
 );
 
 SELECT * FROM wp_bhaa_race_detail;
 DELETE FROM wp_bhaa_race_detail;
 
+INSERT INTO wp_bhaa_race_detail (league,leaguetype,event,eventname,eventdate,race,racetype,distance,unit)
 select 
 l2e.p2p_from as league,
 leaguetype.meta_value as leaguetype,
@@ -33,7 +36,7 @@ LEFT join wp_postmeta racetype on (race.ID=racetype.post_id AND racetype.meta_ke
 LEFT join wp_postmeta racedistance on (race.ID=racedistance.post_id AND racedistance.meta_key='bhaa_race_distance')
 LEFT join wp_postmeta raceunit on (race.ID=raceunit.post_id AND raceunit.meta_key='bhaa_race_unit')
 LEFT join wp_postmeta leaguetype on (l2e.p2p_from=leaguetype.post_id AND leaguetype.meta_key='bhaa_league_type')
-where l2e.p2p_type='league_to_event' and l2e.p2p_from=3103
+where l2e.p2p_type='league_to_event' and l2e.p2p_from IN (3103,3615)
 ORDER BY eventdate;
 
 select DISTINCT(meta_key) from wp_postmeta;
