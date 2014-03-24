a27ae9e999476cf89a15f66917c4d5c0335821ff
emeraldjava <paul.t.oconnell@gmail.com>
2014-03-24 18:07:28 +0000

    Add page to allow a raceresult to be edited.

---
+++ public/views/runner-editresult.php
@@ -0,0 +1,11 @@
+<?php
+if ( !current_user_can( 'manage_options' ) )  {
+	wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+}
+get_header();
+
+echo "<pre>GET "; print_r($_GET); echo "</pre>";
+echo "<pre>POST "; print_r($_POST); echo "</pre>";
+
+get_footer();
+?>
