55748fdf7b9da91e169c8b3bc7dee9efd977def3
emeraldjava <paul.t.oconnell@gmail.com>
2014-04-02 09:54:52 +0100

    Define the table #39

--- sql/wp_bhaa_teamsummary.sql
+++ sql/wp_bhaa_teamsummary.sql
@@ -1,1 +1,10 @@
 -- wp_bhaa_teamsummary
+CREATE TABLE wp_bhaa_teamsummary (
+       race int(11) NOT NULL,
+       team int(11) NOT NULL,
+       teamname varchar(20),
+       totalstd int(11) NOT NULL,
+       class varchar(1) NOT NULL, 
+       position int(11) NOT NULL,
+       leaguepoints double NOT NULL
+);
