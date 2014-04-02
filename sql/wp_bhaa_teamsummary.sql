b6f8fbb014749894746b80925e13d0a118aa4366
emeraldjava <paul.t.oconnell@gmail.com>
2014-04-02 10:01:21 +0100

    Populate the wp_bhaa_teamsummary table #39

--- sql/wp_bhaa_teamsummary.sql
+++ sql/wp_bhaa_teamsummary.sql
@@ -4,7 +4,20 @@ CREATE TABLE wp_bhaa_teamsummary (
        team int(11) NOT NULL,
        teamname varchar(20),
        totalstd int(11) NOT NULL,
-       class varchar(1) NOT NULL, 
        position int(11) NOT NULL,
        leaguepoints double NOT NULL
 );
+
+INSERT INTO wp_bhaa_teamsummary
+SELECT 
+  race,
+  team,
+  teamname,
+  min(totalpos) as totalpos,  
+  min(position)as position,
+  max(leaguepoints) as leaguepoints
+FROM wp_bhaa_teamresult
+WHERE position!=0
+GROUP BY race,team;
+
+SELECT * FROM wp_bhaa_teamsummary;
