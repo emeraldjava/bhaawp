aced8787a70c65c81aede9d46d4ddc2a5318d9a8
emeraldjava <paul.t.oconnell@gmail.com>
2014-04-02 10:11:52 +0100

    Add index #39

--- sql/wp_bhaa_teamsummary.sql
+++ sql/wp_bhaa_teamsummary.sql
@@ -1,12 +1,14 @@
 -- wp_bhaa_teamsummary
+DROP TABLE wp_bhaa_teamsummary;
 CREATE TABLE wp_bhaa_teamsummary (
        race int(11) NOT NULL,
        team int(11) NOT NULL,
        teamname varchar(20),
        totalstd int(11) NOT NULL,
        position int(11) NOT NULL,
-       leaguepoints double NOT NULL
-);
+       leaguepoints double NOT NULL,
+       PRIMARY KEY (race, team) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 INSERT INTO wp_bhaa_teamsummary
 SELECT 
