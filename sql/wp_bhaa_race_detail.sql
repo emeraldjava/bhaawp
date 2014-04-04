308f6a9199effd0aeba62a0c5d9d6f810f275a66
emeraldjava <paul.t.oconnell@gmail.com>
2014-04-04 17:57:58 +0100

    Get the event start_date from events manager table

--- sql/wp_bhaa_race_detail.sql
+++ sql/wp_bhaa_race_detail.sql
@@ -20,13 +20,14 @@ l2e.p2p_from as league,
 leaguetype.meta_value as leaguetype,
 event.ID as event,
 event.post_title as eventname,
-event.post_date as eventdate,
+em.event_start_date as eventdate,
 race.ID as race,
 racetype.meta_value as racetype,
 racedistance.meta_value as distance,
 raceunit.meta_value as raceunit
 from wp_p2p l2e
 join wp_posts event on (l2e.p2p_to=event.ID)
+join wp_em_events em on (event.id=em.post_id)
 join wp_p2p e2r on (l2e.p2p_to=e2r.p2p_from AND e2r.p2p_type='event_to_race')
 join wp_posts race on (e2r.p2p_to=race.ID)
 LEFT join wp_postmeta racetype on (race.ID=racetype.post_id AND racetype.meta_key='bhaa_race_type')
