dfce5123647bb13e1007380f0e2b56273d4fe6ce
emeraldjava <paul.t.oconnell@gmail.com>
2014-07-07 19:18:46 +0100

    update the team summary

--- sql/wp_bhaa_teamsummary.sql
+++ sql/wp_bhaa_teamsummary.sql
@@ -9,17 +9,24 @@ CREATE TABLE wp_bhaa_teamsummary (
        leaguepoints double NOT NULL,
        PRIMARY KEY (race, team) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+ALTER TABLE wp_bhaa_teamsummary ADD COLUMN class varchar(1) DEFAULT NULL AFTER totalstd;
+ALTER TABLE wp_bhaa_teamsummary ADD COLUMN totalpos int(11) DEFAULT NULL AFTER totalstd;
 
+DELETE FROM wp_bhaa_teamsummary
+-- update the team summary table
 INSERT INTO wp_bhaa_teamsummary
 SELECT 
   race,
   team,
   teamname,
+  min(totalstd) as totalstd,  
   min(totalpos) as totalpos,  
+  class,
   min(position)as position,
   max(leaguepoints) as leaguepoints
 FROM wp_bhaa_teamresult
 WHERE position!=0
-GROUP BY race,team;
+GROUP BY race,team
+ORDER BY class,position;
 
 SELECT * FROM wp_bhaa_teamsummary;
