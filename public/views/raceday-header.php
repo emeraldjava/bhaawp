c9dacca8e4cf55979141ca0363d4f3e0fd8d6adc
emeraldjava <paul.t.oconnell@gmail.com>
2014-07-08 18:10:50 +0100

    remove dead header

--- public/views/raceday-header.php
+++ public/views/raceday-header.php
@@ -1,20 +1,4 @@
 <?php 
-$a = '<table style="width:100%">
-<tr style="line-height: 1px; padding-bottom: 1em;">
-<td style="width:33%"><h3><a role="button" class="btn btn-default" href="/raceday-register/">Register</a></h3></td>
-<td style="width:33%"><h3><a role="button" class="btn btn-default" href="/raceday-newmember/">New/Day Member</a></h3></td>		
-<td style="width:33%"><h3><a role="button" class="btn btn-default" href="/raceday-prereg/">Pre-Registered</a></h3></td>
-</tr>
-<tr style="line-height: 14px; padding-bottom: 1em;">
-<td style="width:33%"><h4><a href="/raceday-list/">List</a></h4></td>
-<td style="width:33%"><h4><a href="/raceday-latest/">Latest</a></h4></td>
-<td style="width:33%">
-<a href="/wp-admin/edit.php?post_type=race&action=bhaa-raceday-export">Export</a>
-</h4></td>
-</tr>
-</table>
-<hr/>';
-
 $buttonMenu = '
 <div class="btn-group btn-group-justified">
 <a class="btn btn-primary" href="/raceday-register" role="button">BHAA Member</a>
