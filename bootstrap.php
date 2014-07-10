f41b1296ed1abbe5a7d5d33ef18f9a3693aeb3ef
emeraldjava <paul.t.oconnell@gmail.com>
2014-07-10 18:20:16 +0100

    Rename class cpt

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ define( 'BHAA_PLUGIN_DIR' , dirname(__FILE__) );
 // admin
 require_once(BHAA_PLUGIN_DIR.'/admin/class-runner-admin.php');
 require_once(BHAA_PLUGIN_DIR.'/admin/class-event-admin.php');
-require_once(BHAA_PLUGIN_DIR.'/admin/RaceCpt.php');
+require_once(BHAA_PLUGIN_DIR.'/admin/class-race-cpt.php');
 require_once(BHAA_PLUGIN_DIR.'/admin/WPFlashMessages.php');
 
 // 3rd party libs
