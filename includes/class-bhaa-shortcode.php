0c6874743ab3d43456d80650b3d480f29d187d2d
emeraldjava <paul.t.oconnell@gmail.com>
2014-09-08 18:23:30 +0100

    update 'bhaa_runner' shortcode logic

--- includes/class-bhaa-shortcode.php
+++ includes/class-bhaa-shortcode.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Handles the BHAA shortcodes
+ * @author oconnellp
+ */
 class Bhaa_Shortcode{
 	
 	protected static $instance = null;
@@ -12,6 +16,16 @@ class Bhaa_Shortcode{
 	}
 	
 	private function __construct() {
+	}
+	
+	/**
+	 * Return a specific 'runner_id' race results
+	 * @param unknown $args
+	 */
+	function bhaa_runner($args) {
+		$this_runner = get_query_var('runner_id');
+		//error_log('this_runner '.$this_runner);
+		echo RaceResult_List_Table::get_instance()->renderRunnerTable($this_runner);
 	}
 }
 ?>
