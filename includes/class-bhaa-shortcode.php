23bceaafffb8137bbfe8c7348420c394424ffb38
emeraldjava <paul.t.oconnell@gmail.com>
2014-09-16 12:08:19 +0100

    Add extra runner shortcodes

--- includes/class-bhaa-shortcode.php
+++ includes/class-bhaa-shortcode.php
@@ -20,6 +20,11 @@ class Bhaa_Shortcode{
 	
 	function registerShortCodes(){
 		add_shortcode('bhaa_runner',array(Bhaa_Shortcode::get_instance(),'bhaa_runner'));
+		add_shortcode('bhaa_runner_name',array(Bhaa_Shortcode::get_instance(),'bhaa_runner_name'));
+		add_shortcode('bhaa_runner_standard',array(Bhaa_Shortcode::get_instance(),'bhaa_runner_standard'));
+		add_shortcode('bhaa_runner_status',array(Bhaa_Shortcode::get_instance(),'bhaa_runner_status'));
+		add_shortcode('bhaa_runner_company_name',array(Bhaa_Shortcode::get_instance(),'bhaa_runner_company_name'));
+		add_shortcode('bhaa_runner_results',array(Bhaa_Shortcode::get_instance(),'bhaa_runner_results'));
 		add_shortcode('house_title',array(Bhaa_Shortcode::get_instance(),'house_title'));
 		add_shortcode('house_sector',array(Bhaa_Shortcode::get_instance(),'house_sector'));
 	}	
@@ -28,10 +33,34 @@ class Bhaa_Shortcode{
 	 * Return a specific 'runner_id' race results
 	 * @param unknown $args
 	 */
-	function bhaa_runner($args) {
-		$this_runner = get_query_var('id');//'runner_id'
-		//error_log('this_runner '.$this_runner);
-		return RaceResult_List_Table::get_instance()->renderRunnerTable(7713);//$this_runner);
+	function bhaa_runner($atts) {
+		$bhaaid = get_query_var('bhaaid');
+		$runner = new Runner($bhaaid);
+		return $runner->getFullName();
+	}
+	
+	function bhaa_runner_name($atts) {
+		$runner = new Runner(get_query_var('bhaaid'));
+		return $runner->getFullName();
+	}
+	
+	function bhaa_runner_standard($atts) {
+		$runner = new Runner(get_query_var('bhaaid'));
+		return $runner->getStandard();
+	}
+	
+	function bhaa_runner_status($atts) {
+		$runner = new Runner(get_query_var('bhaaid'));
+		return $runner->getStatus();
+	}
+	
+	function bhaa_runner_company_name($atts) {
+		$runner = new Runner(get_query_var('bhaaid'));
+		return $runner->getCompanyName();
+	}
+	
+	function bhaa_runner_results($args) {
+		return RaceResult_List_Table::get_instance()->renderRunnerTable(get_query_var('bhaaid'));
 	}
 	
 	function post_title($args) {
