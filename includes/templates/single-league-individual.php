ccb3b8d2ba926573d3d3495c0cc8e6bd29961fd8
emeraldjava <paul.t.oconnell@gmail.com>
2014-09-16 15:25:38 +0100

    update the league template to use the enfold shortcodes.

--- includes/templates/single-league-individual.php
+++ includes/templates/single-league-individual.php
@@ -4,19 +4,18 @@ get_header();
 echo '<section id="primary">';
 echo the_title('<h1>','</h1>');
 
-echo do_shortcode('[one_third last="no"][bhaa_league division=A top=10][/one_third]');
-echo do_shortcode('[one_third last="no"][bhaa_league division=B top=10][/one_third]');
-echo do_shortcode('[one_third last="yes"][bhaa_league division=L1 top=10][/one_third]');
+echo do_shortcode('[av_one_third first][av_textblock][bhaa_league division=A top=10][/av_textblock][/av_one_third]');
+echo do_shortcode('[av_one_third ][av_textblock][bhaa_league division=B top=10][/av_textblock][/av_one_third]');
+echo do_shortcode('[av_one_third ][av_textblock][bhaa_league division=L1 top=10][/av_textblock][/av_one_third]');
 
-echo do_shortcode('[one_third last="no"][bhaa_league division=C top=10][/one_third]');
-echo do_shortcode('[one_third last="no"][bhaa_league division=D top=10][/one_third]');
-echo do_shortcode('[one_third last="yes"][bhaa_league division=L2 top=10][/one_third]');
+echo do_shortcode('[av_one_third first][av_textblock][bhaa_league division=C top=10][/av_textblock][/av_one_third]');
+echo do_shortcode('[av_one_third ][av_textblock][bhaa_league division=D top=10][/av_textblock][/av_one_third]');
+echo do_shortcode('[av_one_third ][av_textblock][bhaa_league division=L2 top=10][/av_textblock][/av_one_third]');
 
-echo do_shortcode('[one_half last="no"][bhaa_league division=E top=10][/one_half]');
-echo do_shortcode('[one_half last="yes"][bhaa_league division=F top=10][/one_half]');
+echo do_shortcode('[av_one_half first][av_textblock][bhaa_league division=E top=10][/av_textblock][/av_one_half]');
+echo do_shortcode('[av_one_half ][av_textblock][bhaa_league division=F top=10][/av_textblock][/av_one_half]');
 
-echo do_shortcode('[separator top="40" style="single"]');
-
+//echo do_shortcode('[separator top="40" style="single"]');
 echo '</section>';
 
 if($data['blog_comments']):
