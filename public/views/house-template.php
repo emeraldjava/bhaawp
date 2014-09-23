db2a6638a7996820cfa122b570b7b36b3f521dd2
emeraldjava <paul.t.oconnell@gmail.com>
2014-09-23 16:05:57 +0100

    use the template single-house.php file instead.

--- public/views/house-template.php
+++ public/views/house-template.php
@@ -1,9 +1,29 @@
 <?php get_header(); ?>
-<section id="primary">
+
 <?php 
+// use the template single-house instead
+
 // This template gets the page content for a the house CPT.
 $page = get_page_by_title('house_template_page');
 $content = apply_filters('the_content', $page->post_content);
+?>
+
+<div class='container_wrap main_color <?php avia_layout_class( 'main' ); ?>'>
+
+<div class='container'>
+
+<div class='template-page content  <?php avia_layout_class( 'content' ); ?> units'>
+<?php
 echo $content;
 ?>
-</section>
+</div><!--end content-->
+
+<?php 
+//get the sidebar
+$avia_config['currently_viewing'] = 'page';
+get_sidebar();
+?>
+</div><!--end container-->
+			
+</div><!--end container-->
+<?php get_footer(); ?>
