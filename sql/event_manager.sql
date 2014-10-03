3eaef730f3304a71059a3d44f34d404aebfd95ac
emeraldjava <paul.t.oconnell@gmail.com>
2014-10-03 20:03:15 +0100

    update the pre race page format

--- sql/event_manager.sql
+++ sql/event_manager.sql
@@ -1,57 +1,99 @@
-
-update wp_options
-set option_value='<div eventid="#_EVENTID" postid="#_EVENTPOSTID">
-[av_tab_container position=top_tab boxed=border_tabs initial=1]
-
-{is_future}
-[av_tab title="#_EVENTNAME" icon_select="yes" icon="6"]
+[av_tab title='#_EVENTNAME' icon_select='yes' icon='6']
 <p>
 	<strong>Date/Time</strong><br/>
 	Date(s) - #_EVENTDATES<br /><i>#_EVENTTIMES</i>
 </p>
 [av_one_full first]
-[av_hr class="default" height="50" shadow="no-shadow" position="center"]
+[av_hr class='default' height='50' shadow='no-shadow' position='center']
 [/av_one_full]
 #_EVENTNOTES
 [av_one_full first]
-[av_hr class="default" height="50" shadow="no-shadow" position="center"]
+[av_hr class='default' height='50' shadow='no-shadow' position='center']
 [/av_one_full]
-[two_third last="no"]<strong>Location</strong> : #_LOCATIONNAME
-#_LOCATIONFULLBR
-#_LOCATIONNOTES
+[two_third last='no']
 [/two_third]
-[one_third last="yes"]
-#_LOCATIONMAP
+[one_third last='yes']
+
 [/one_third]
 [/av_tab]
 
-[av_tab title="Register" icon_select="yes" icon="5"]
-#_BOOKINGFORM
+[av_tab title='Register' icon_select='yes' icon='5']
+
 [/av_tab]
+
+update wp_options
+set option_value="<div eventid='#_EVENTID' postid='#_EVENTPOSTID'>
+{is_future}
+[av_one_full first]
+[av_heading heading='#_EVENTNAME' tag='h2' color='custom-color-heading' custom_font='#81d742' style='blockquote classic-quote' size='' subheading_active='subheading_below' subheading_size='15' padding='10' custom_class='']
+Date(s) - #_EVENTDATES<br /><i>#_EVENTTIMES</i>
+[/av_heading]
+[/av_one_full]
+
+[av_one_half first]
+[av_toggle_container initial='0' mode='accordion' sort='' custom_class='']
+[av_toggle title='#_EVENTDATES' tags='']
+<strong>Date/Time</strong><br/>Date(s) - #_EVENTDATES<br /><i>#_EVENTTIMES</i>
+[/av_toggle]
+[av_toggle title='Where/Location' tags='']
+#_LOCATIONNAME
+[/av_toggle]
+[av_toggle title='Contact' tags='']
+Contact Person
+[/av_toggle]
+[av_toggle title='WHO' tags='']
+Contact Person
+[/av_toggle]
+[av_toggle title='Toggle 2' tags='']
+#_EVENTNOTES
+[/av_toggle]
+[/av_toggle_container]
+[/av_one_half]
+
+[av_one_half]
+[av_icon_box icon='ue800' font='entypo-fontello' position='left' title='Register' link='' linktarget='' linkelement='' custom_class='']
+#_BOOKINGFORM
+[/av_icon_box]
+[/av_one_half]
+
+[av_one_half first]
+[av_textblock custom_class='']
+<strong>Location</strong> : #_LOCATIONNAME
+#_LOCATIONFULLBR
+#_LOCATIONNOTES
+[/av_textblock]
+[/av_one_half]
+
+[av_one_half]
+<strong>Location</strong> : #_LOCATIONNAME
+#_LOCATIONFULLBR
+#_LOCATIONNOTES
+
+#_LOCATIONMAP
+[/av_one_half]
 {/is_future}
 
 {is_past}
-[av_tab title="Results"]#_BHAARACERESULTS[/av_tab]
-[av_tab title="Teams"]#_BHAATEAMRESULTS[/av_tab]
-[av_tab title="#_EVENTNAME"]
+[av_tab title='Results']#_BHAARACERESULTS[/av_tab]
+[av_tab title='Teams']#_BHAATEAMRESULTS[/av_tab]
+[av_tab title='#_EVENTNAME']
 <p>
 	<strong>Date/Time</strong><br/>
 	Date(s) - #_EVENTDATES<br /><i>#_EVENTTIMES</i>
 </p>
-[separator top="20" style="single"]
+[separator top='20' style='single']
 #_EVENTNOTES
 [/av_tab]
 {/is_past}			
-
-[av_tab title="Standards" icon_select="yes" icon="4"]
+[av_tab title='Standards' icon_select='yes' icon='4']
 <h3>BHAA Standard Table</h3>
 <p>Like a golf handicap the BHAA standard table gives a runner a target time for the race distance</p>
 <p>#_BHAASTANDARDS</p>
 [/av_tab]
 [/av_tab_container]
-</div>' where option_name='dbem_single_event_format';
+</div>" where option_name='dbem_single_event_format';
 
-
+-- <h3>#s</h3>
 update wp_options
 set option_value="[av_one_full first]
 [av_heading heading='#s' tag='h3' color='' custom_font='' style='' size='' subheading_active='' subheading_size='15' padding='10' custom_class='']
