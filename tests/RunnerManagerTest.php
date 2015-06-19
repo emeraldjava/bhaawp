9b7d23748af865d7c147e5ee14dde55ed0ac1026
emeraldjava <paul.t.oconnell@gmail.com>
2015-06-19 17:35:35 +0100

    RunnerManager test

---
+++ tests/RunnerManagerTest.php
@@ -0,0 +1,20 @@
+<?php
+ /**
+ * Runner test case.
+ */
+class RunnerManagerTest extends WP_UnitTestCase {
+
+    private $runnerManager;
+
+    function setUp() {
+        $this->runnerManager = Runner_Manager::get_instance();
+    }
+
+    function testRunnerExistsTrue() {
+        $this->assertEquals(1,$this->runnerManager->runnerExists(7713));
+    }
+
+    function testRunnerExistsFalse() {
+        $this->assertEquals(0,$this->runnerManager->runnerExists(50000));
+    }
+}
