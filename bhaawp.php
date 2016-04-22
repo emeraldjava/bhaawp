6c085c4a9e33fc55ccc95b47126c551926641306
emeraldjava <paul.t.oconnell@gmail.com>
2016-04-22 19:35:58 +0100

    Update version and remove un-needed require_once calls.

--- bhaawp.php
+++ bhaawp.php
@@ -3,7 +3,7 @@
 Plugin Name:        BHAA Plugin
 Plugin URI:         https://github.com/emeraldjava/bhaawp
 Description:        Plugin for the Business House Athletic Association which handle user registration, race  results and leagues.
-Version:            2016.04.03
+Version:            2016.04.22
 Author:             paul.t.oconnell@gmail.com
 Author URI:         https://github.com/emeraldjava
 Text Domain:        bhaawp
@@ -30,7 +30,7 @@ define( 'BHAA_PLUGIN_DIR' , dirname(__FILE__) );
  * Public-Facing Functionality
 *----------------------------------------------------------------------------*/
 
-require_once( plugin_dir_path( __FILE__ ) . 'public/class-bhaa.php' );
+//require_once( plugin_dir_path( __FILE__ ) . 'public/class-bhaa.php' );
 
 /*
  * Register hooks that are fired when the plugin is activated or deactivated.
@@ -49,7 +49,7 @@ add_action( 'plugins_loaded', array( 'Bhaa', 'get_instance' ) );
  * Load the admin
 */
 if ( is_admin() ) { //&& ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
-  require_once( plugin_dir_path( __FILE__ ) . 'admin/class-bhaa-admin.php' );
+  //require_once( plugin_dir_path( __FILE__ ) . 'admin/class-bhaa-admin.php' );
   add_action( 'plugins_loaded', array( 'Bhaa_Admin', 'get_instance' ) );
 }
 ?>
