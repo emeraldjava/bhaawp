ed2341bf8799fb797d62fd0d7a49f04cd21807aa
emeraldjava <paul.t.oconnell@gmail.com>
2016-04-22 19:46:30 +0100

    Add import url links

--- public/templates/admin.php
+++ public/templates/admin.php
@@ -5,7 +5,15 @@
  */
 include_once 'header.php';
 ?>
-<h1><?php _e( 'BHAA Raceday Admin.'); ?></h1>
+
+<?php
+$event = Raceday::get_instance()->getEvent();
+echo '<h2>BHAA RACE DAY ADMIN</h2>';
+echo '<h3>Actions</h3>';
+echo sprintf('<h3><a href="/raceday-admin?action=preregimport&eventid=%d&raceid=%d">Import PRE_REG</a></h3>',$event->event_id,$event->race);
+echo sprintf('<h3><a href="/raceday-admin?action=preregexport&eventid=%d&raceid=%d">Export PRE_REG</a></h3>',$event->event_id,$event->race);//119,2851);
+echo sprintf('<h3><a href="/raceday-admin?action=deleteall&eventid=%d&raceid=%d">Delete All RACE_REG</a></h3>',$event->event_id,$event->race);//119,2851);
+?>
 <?php
 include_once 'footer.php';
 ?>
