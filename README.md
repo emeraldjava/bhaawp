9d56b07b0054ebf052e4878a1e436c1a50f258de
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-01-30 15:39:30 +0000

    2017.01.30 Format the day runner date of birth. Add top-ten league csv file export feature.

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ Shortcodes
 
 Change Log
 ==========
+- 2017.01.30 Format the day runner date of birth. Add top-ten league csv file export feature.
 - 2017.01.27 Updates to the runner json export endpoint.
 - 2017.01.26 Add page to list runners by standard.
 - 2017.01.25 Update to use mustache templates in more places.
--- bhaawp.php
+++ bhaawp.php
@@ -3,7 +3,7 @@
 Plugin Name:        BHAA Plugin
 Plugin URI:         https://github.com/emeraldjava/bhaawp
 Description:        Plugin for the Business House Athletic Association which handle user registration, race  results and leagues.
-Version:            2017.01.27
+Version:            2017.01.30
 Author:             paul.t.oconnell@gmail.com
 Author URI:         https://github.com/emeraldjava
 Text Domain:        bhaawp
