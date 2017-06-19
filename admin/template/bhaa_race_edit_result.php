046b1d5c520101caaab2b5e43862c33a29a595a3
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-06-19 15:43:51 +0100

    Make the result and race fields non-editable

--- admin/template/bhaa_race_edit_result.php
+++ admin/template/bhaa_race_edit_result.php
@@ -2,9 +2,9 @@
 echo '<div class="wrap"><h2>Edit Race Result '.$_GET['raceresult'].' '.$raceLink.'</h2></div>';
 echo '<form action="'.$link.'" method="POST">
     <fieldset>
-        <label>Result:<input type="text" id="bhaa_raceresult_id" name="bhaa_raceresult_id" value="'.$raceResult->id.'"/></label><br>
+        <label>Result:<input type="text" readonly id="bhaa_raceresult_id" name="bhaa_raceresult_id" value="'.$raceResult->id.'"/></label><br>
         <label>Runner:<input type="text" id="bhaa_runner" name="bhaa_runner" value="'.$raceResult->runner.'"/></label><br>
-        <label>Race:<input type="text" id="bhaa_race" name="bhaa_race" value="'.$raceResult->race.'"/></label><br>
+        <label>Race:<input type="text" readonly id="bhaa_race" name="bhaa_race" value="'.$raceResult->race.'"/></label><br>
         <label>Time:<input type="text" id="bhaa_time" name="bhaa_time" value="'.$raceResult->racetime.'"/></label><br>
         <label>Pre Std:<input type="text" id="bhaa_pre_standard" name="bhaa_pre_standard" value="'.$raceResult->standard.'"/></label><br>
         <label>Post Std:<input type="text" id="bhaa_post_standard" name="bhaa_post_standard" value="'.$raceResult->poststandard.'"/></label><br>
