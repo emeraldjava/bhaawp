01a03a98f5cc6f75de017381cac86d5202b3d113
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-06-19 15:56:39 +0100

    Display the race title on the edit page.

--- admin/template/bhaa_race_edit_results.php
+++ admin/template/bhaa_race_edit_results.php
@@ -1,6 +1,7 @@
 <?php
 $link = admin_url('admin.php');
-echo '<div class="wrap"><h2>Edit Race Results Template '.$_GET['id'].'</h2>
+$race = get_the_title($_GET['id']);
+echo '<div class="wrap"><h2>Edit Race Results : '.$race.'</h2>
     <form method="post" action="'.$link.'">
 	<input type="hidden" name="action" value="bhaa_race_add_result" />
 		<input type="hidden" name="post_id" value="'.$_GET['id'].'"/>
